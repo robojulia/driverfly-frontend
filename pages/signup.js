f1c3f2f5fd1abd829a9335077790b55cf80fe780
Muhammad Irfan <irfan@codeupscale.com>
2022-02-17 15:38:04 +0500

    DIRV-23

--- pages/signup.js
+++ pages/signup.js
@@ -28,24 +28,39 @@ export default function Signup()
                 </div>
             </div>
             <div className="container">
-                <div className={SignupStyle.form}>
-                    <h2 className="text-center">Create New Driver Account</h2>
-                    <form>
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Email address</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                        </div>
-                        <div class="form-group">
-                            <label for="exampleInputPassword1">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
-                        </div>
-                        <div class="form-group form-check">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+                <div class="row">
+                    <div class="col-lg-2">
+                    </div>
+                    <div class="col-lg-8">
+                    <div className={SignupStyle.form}>
+                        <h2 className="text-center my-5">Create New Driver Account</h2>
+                        <form className="my-5">
+                           <div class="form-group">
+                                <input type="ematextil" class="form-control" id="exampleInputUsername" aria-describedby="emailHelp" placeholder="User Name" />
+                            </div>
+                            <div class="form-group">
+                                <input type="email" class="form-control" id="exampleInputUsername" aria-describedby="emailHelp" placeholder="Email" />
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Confirm Password" />
+                            </div>
+                            <div class="form-group">
+                                <input type="tel" class="form-control" id="exampleInputPhone" placeholder="Phone" />
+                            </div>
+                            <div class="form-group form-check">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+                                <label class="form-check-label" for="exampleCheck1">You accept our <a href="" className={SignupStyle.link}>Terms and Conditions and Privacy Policy</a></label>
+                            </div>
+                            <button type="submit" class="btn btn-dark w-100 d-block p-3 my-5">Register now</button>
+                        </form>
+                     </div>
+                    </div>
+                    <div class="col-lg-2">
+                    </div>
+                    
                 </div>
             </div>
         </>
