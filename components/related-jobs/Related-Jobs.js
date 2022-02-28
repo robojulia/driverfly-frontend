ece7834e6fd6a168f933f27610cfa8f9623e1c07
Muhammad Irfan <irfan@codeupscale.com>
2022-02-28 15:57:11 +0500

    DRIV-32 Create Component for Related Jobes in Find Jobes detail Page

---
+++ components/related-jobs/Related-Jobs.js
@@ -0,0 +1,96 @@
+export default function RelatedJobs() {
+
+    return (
+        <>
+            <div className="related-job-sec">
+                <h3>Related Jobs</h3>
+                <div className="row mt-3">
+                    <div className="col-md-12">
+                        <div className="media align-items-center ">
+                            <label className="checkbox-inline" for="remember">
+                                <input type="checkbox" name="remember" id="remember" value="1" />
+
+                            </label>
+                            <img className="d-flex mr-4 truck-img" src="img/CTR-logo-cartoon.png" alt="" />
+                            <div className="media-body">
+                                <span className="urgent">URGENT</span>
+                                <h6>Solo</h6>
+                                <h4 className="mt-0">CDL-A Regional/OTR – No Touch Freight, Live Load/Unload<span className="d-block mt-2" data-toggle="tooltip"
+                                    data-placement="top" title="Tooltip on top"> <i className="fa fa-star" aria-hidden="true"></i> </span></h4>
+                                <div className="job-date-author">
+                                    posted 3 days ago
+                                    by <a href="" className="employer text-theme">Custom Trucker Recruiting</a>
+                                </div>
+                                <div className="job-metas">
+                                    <div className="job-location">
+                                        <i className="fa fa-star-o" aria-hidden="true"></i><strong>Accepting drivers from anywhere in the Illinois, Indiana, Michigan and Ohio</strong>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <button type="button" className="btn btn-outline-danger"><i className="fa fa-star-o m-0" aria-hidden="true"></i></button>
+
+                        </div>
+
+                        <div className="media align-items-center shadow-sm">
+                            <label className="checkbox-inline" for="remember">
+                                <input type="checkbox" name="remember" id="remember" value="1" />
+
+                            </label>
+                            <img className="d-flex mr-4 truck-img" src="img/CTR-logo-cartoon.png" alt="" />
+                            <div className="media-body">
+                                <span className="urgent">URGENT</span>
+                                <h6>Solo</h6>
+                                <h4 className="mt-0">Class A CDL OTR Truck Drivers (W-2)<span className="" data-toggle="tooltip"
+                                    data-placement="top" title="Tooltip on top"> <i className="fa fa-star" aria-hidden="true"></i> </span></h4>
+                                <div className="job-date-author">
+                                    posted 3 days ago
+                                    by <a href="" className="employer text-theme">Custom Trucker Recruiting</a>
+                                </div>
+                                <div className="job-metas">
+                                    <div className="job-location">
+                                        <i className="fa fa-map-marker mr-2" aria-hidden="true"></i>Burnsville, MN
+                                    </div>
+                                    <div className="job-location">
+                                        <i className="fa fa-star-o" aria-hidden="true"></i><strong>Accepting drivers from anywhere in Illinois, Indiana, Iowa, Kansas, Michigan, Minnesota, Missouri, Nebraska, North Dakota, Ohio, South Dakota and Wisconsin</strong>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <button type="button" className="btn btn-outline-danger"><i className="fa fa-star-o m-0" aria-hidden="true"></i></button>
+
+                        </div>
+                        <div className="media align-items-center shadow-sm">
+                            <label className="checkbox-inline" for="remember">
+                                <input type="checkbox" name="remember" id="remember" value="1" />
+
+                            </label>
+                            <img className="d-flex mr-4 truck-img" src="img/ACWNCW-logo_PNG.jpg" alt="" />
+                            <div className="media-body">
+                                <span className="urgent">URGENT</span>
+                                <h6>Solo</h6>
+                                <h4 className="mt-0">CDL-A Home Daily – Roll Off Container Trucks<span className="" data-toggle="tooltip"
+                                    data-placement="top" title="Tooltip on top"> <i className="fa fa-star" aria-hidden="true"></i> </span></h4>
+                                <div className="job-date-author">
+                                    posted 3 days ago
+                                    by <a href="" className="employer text-theme">National Concrete Washout</a>
+                                </div>
+                                <div className="job-metas">
+                                    <div className="job-location">
+                                        <i className="fa fa-map-marker mr-2" aria-hidden="true"></i>Bloomington, CA and Fort Worth, TX
+                                    </div>
+                                    <div className="job-location">
+                                        <i className="fa fa-star-o" aria-hidden="true"></i><strong>Accepting drivers from anywhere in the CaliforniaTexas</strong>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <button type="button" className="btn btn-outline-danger"><i className="fa fa-star-o m-0" aria-hidden="true"></i></button>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
