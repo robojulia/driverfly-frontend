f9f68bdc70e7ebc1c41bdef192d31199a130571d
Muhammad Irfan <irfan@codeupscale.com>
2022-03-01 14:40:39 +0500

    DRIV-35

--- pages/blog-single.js
+++ pages/blog-single.js
@@ -18,10 +18,10 @@ export default function Detail() {
                             < RelatedJobs /> */}
                             <img src="img/Freight1.jpg" className='w-100' />
                             <p className='mt-3'>Changes in Trucking</p>
-                            <h2 className='font-weight-normal my-4'> What Is This New ELDT Program You Speak Of?</h2>
-                            <div class="top-info">
-                                <a href=""><i class="fa fa-clock-o" aria-hidden="true"></i>January 26, 2022</a>
-                                <span class="comments"><FontAwesomeIcon icon="fa-solid fa-comment" />0 Comments</span>
+                            <h2 className='font-weight-normal'> What Is This New ELDT Program You Speak Of?</h2>
+                            <div className="top-info mb-3">
+                                <a href="" className='text-secondary'><i className="fa fa-clock-o" aria-hidden="true"></i> January 26, 2022</a>
+                                <span className="comments">  <i className="fa fa-comment ml-4"></i>  0 Comments</span>
                             </div>
                             <p >Starting February 7, 2022, the Entry-Level Driver Training (ELDT) regulations will be changed, now requiring all aspiring CDL drivers to receive training from providers listed on the FMSCA’s Training Provider Registry before they can take their CDL skills test. </p>
                             <h2 className='font-weight-normal my-4'>Are you affected?</h2>
@@ -41,6 +41,44 @@ export default function Detail() {
                             <p>If you’re a motor carrier looking to be listed on the TPR, visit  <Link href="" ><a>https://tpr.fmcsa.dot.gov/Provider</a></Link> for more information on the requirements and registration process.</p>
                             <h2 className='font-weight-normal my-4'>Resources</h2>
                             <p>Of course, this is just an overview of the new ELDT regulations. For the official source of information, refer to <Link href=''><a>FMCSA’s site</a></Link> or the <Link href=''><a>Code of Federal Regulations.</a></Link></p>
+
+
+
+                            <div className="comment mb-5">
+                                <h3>Leave a Comment</h3>
+                                <p>Your email address will not be published.</p>
+                                <div className={BlogDetail.comment_form}>
+                                    <div className="container">
+                                        <div className="row p-5">
+                                            <div className="col contact-outer">
+                                                <form action="" method="post">
+                                                    <div className="form-group">
+                                                        <label>Name</label>
+                                                        <div className="col p-0"><input type="text" className="form-control  p-4" name="your_name" required="required" /></div>
+                                                    </div>
+                                                    <div className="form-group ">
+                                                        <label>Email</label>
+                                                        <div className="col p-0"> <input type="email" className="form-control p-4" name="email" required="required" /></div>
+                                                    </div>
+                                                    <div className="form-check mb-3">
+                                                        <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                                                            <label className="form-check-label" for="flexCheckDefault">
+                                                            Save my name, email, and website in this browser for the next time I comment.
+                                                            </label>
+                                                    </div>
+                                                    <div className="form-group">
+                                                        <label>Comment</label>
+                                                        <textarea name="message" id="message" cols="20" rows="6" className="form-control  p-4" ></textarea>
+                                                    </div>
+                                                    <div className="form-group">
+                                                        <button type="submit" className="btn btn-danger py-3 px-5s">Submit Comment </button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <div className="col-lg-4">
                             {/* < JonInformation /> */}
