ac02653df60f035373e4dc8c14dfba991871a179
Jawad Ali <alijamil734@gmail.com>
2022-03-04 23:50:00 +0500

    reset-password and forgot-password form with validation

--- pages/forgot-password.js
+++ pages/forgot-password.js
@@ -1,60 +1,80 @@
-import Link from 'next/link';
-import Breadcrumbs from 'nextjs-breadcrumbs';
-import Back from '../components/back-to-login/Back-Login';
-import Layout from "../components/layouts";
+import Link from 'next/link'
+import Breadcrumbs from 'nextjs-breadcrumbs'
+import { useState } from "react"
+import Back from '../components/back-to-login/Back-Login'
+import Layout from "../components/layouts"
 import Forgotpassword from '../public/css/Forgot.module.css'
 
-export default function Forgot() {
-    return (
-        <>
+export default function Forgot () {
+
+  const [username, setUsername] = useState("")
+const [error, setError] = useState("")
+
+  const submitHandler = e => {
+    e.preventDefault()
+    if (!username) {
+      setError( "Username or email is required")
+      return
+    }
+    // clear error
+    if (error) {
+      setError("")
+    }
+    // TODO: send email to user to reset password
+    console.log(username);
+  }
+
+  return (
+    <>
 
 
-            <div className="top-links-sec">
-                <div className="container">
-                    <div className="top-links-inner d-flex align-items-center justify-content-between">
-                        <h2>Forgot Password</h2>
-                        < Breadcrumbs />
-                    </div>
+      <div className="top-links-sec">
+        <div className="container">
+          <div className="top-links-inner d-flex align-items-center justify-content-between">
+            <h2>Forgot Password</h2>
+            < Breadcrumbs />
+          </div>
+        </div>
+      </div>
+      <div className={Forgotpassword.formsec}>
+        <div className="container">
+          <div className='row'>
+            <div className='col-lg-2'></div>
+            <div className='col-lg-8'>
+              < Back />
+              <h4 className='text-center mt-5 font-weight-normal'>Reset Password</h4>
+              <p className="mt-2 mb-5 text-center  text-secondary ">Please Enter Username or Email</p>
+              <form onSubmit={submitHandler} className={Forgotpassword.mb}>
+                <div className="form-group">
+                  <input value={username} onChange={(e) => setUsername(e.target.value)} type="text" className="form-control py-4" placeholder="Username or E-mail" id="useremail" />
+                  <p style={{ fontStyle: "italic", color: "red" }}>{error}</p>
                 </div>
+
+
+                <button type="submit" className={Forgotpassword.success_btn}>Get New Password</button>
+                <Link href="/login">
+                  <button className={Forgotpassword.danger_btn}>
+                    Cancel </button>
+                </Link>
+                <Link href="/login">
+                  <div><a href="" className={Forgotpassword.backlink}>Back To Login</a></div>
+                </Link>
+              </form>
+
             </div>
-            <div className={Forgotpassword.formsec}>
-                <div className="container">
-                    <div className='row'>
-                        <div className='col-lg-2'></div>
-                        <div className='col-lg-8'>
-                            < Back />
-                            <h4 className='text-center mt-5 font-weight-normal'>Reset Password</h4>
-                            <p className="mt-2 mb-5 text-center  text-secondary ">Please Enter Username or Email</p>
-                            <form action="/action_page.php" className={Forgotpassword.mb}>
-                                <div className="form-group">
-                                    <input type="text" className="form-control py-4" placeholder="Username or E-mail" id="useremail" />
-                                </div>
+            <div className='col-lg-2'></div>
+          </div>
+        </div>
+      </div>
 
 
-                                <button type="submit" className={Forgotpassword.success_btn}>Get New Password</button>
-                                <Link href="/login">
-                                    <button type="submit" className={Forgotpassword.danger_btn}>
-                                        Cancel </button>
-                                </Link>
-                                <Link href="/login">
-                                <div><a href="" className={Forgotpassword.backlink}>Back To Login</a></div>
-                                </Link>
-                            </form>
-
-                        </div>
-                        <div className='col-lg-2'></div>
-                    </div>
-                </div>
-            </div>
-
-
-        </>
-    )
+    </>
+  )
 }
-Forgot.getLayout = function getLayout(page) {
-    return (
-        <Layout>
-            {page}
-        </Layout>
-    )
+Forgot.getLayout = function getLayout ( page ) {
+  return (
+    <Layout>
+      {page}
+    </Layout>
+  )
 }
--- pages/reset-password.js
+++ pages/reset-password.js
@@ -1,63 +1,103 @@
-import Link from 'next/link';
-import Breadcrumbs from 'nextjs-breadcrumbs';
-import Back from '../components/back-to-login/Back-Login';
-import Layout from "../components/layouts";
+import Link from 'next/link'
+import Breadcrumbs from 'nextjs-breadcrumbs'
+import { useState } from "react"
+import Back from '../components/back-to-login/Back-Login'
+import Layout from "../components/layouts"
 import Forgotpassword from '../public/css/Forgot.module.css'
 
-export default function Forgot() {
-    return (
-        <>
+export default function Forgot () {
+  const [password, setPassword] = useState("")
+  const [passwordError, setPasswordError] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+  const [confirmPasswordError, setConfirmPasswordError] = useState("")
+
+  const submitHandler  = e => {
+    e.preventDefault()
+    // validate password
+    if(!password){
+      setPasswordError("password is required")
+    }
+
+    // confirm password
+    if(!confirmPassword){
+      setConfirmPasswordError("password confirmation is required")
+    }else{
+      // confrimation password is given, make sure it matches with password
+      if (confirmPassword !== password) {
+        setConfirmPasswordError("password does not match confirmation password")
+      }
+    }
+
+    // clear errors
+    if (passwordError || confirmPasswordError) {
+      setPasswordError("")
+      setConfirmPasswordError("")
+    }
+
+    // if no errors, submit form
+    console.log({
+      password,
+      confirmPassword
+    });
+
+  
+  }
+
+  return (
+    <>
 
 
-            <div className="top-links-sec">
-                <div className="container">
-                    <div className="top-links-inner d-flex align-items-center justify-content-between">
-                        <h2>Reset Password</h2>
-                        < Breadcrumbs />
-                    </div>
+      <div className="top-links-sec">
+        <div className="container">
+          <div className="top-links-inner d-flex align-items-center justify-content-between">
+            <h2>Reset Password</h2>
+            < Breadcrumbs />
+          </div>
+        </div>
+      </div>
+      <div className={Forgotpassword.formsec}>
+        <div className="container">
+          <div className='row'>
+            <div className='col-lg-2'></div>
+            <div className='col-lg-8'>
+
+              <h4 className='text-center mt-5 font-weight-normal'>Reset Password</h4>
+              <p className="mt-2 mb-5 text-center  text-secondary ">Please Enter Your New Password</p>
+              <form onSubmit={submitHandler} className={Forgotpassword.mb}>
+                <div className="form-group">
+                  <input value={password} onChange={(e) => setPassword(e.target.value)}  type="password" className="form-control py-4" placeholder="Password" id="password" />
+                  <p style={{ fontStyle: "italic", color: "red" }}>{passwordError}</p>
                 </div>
-            </div>
-            <div className={Forgotpassword.formsec}>
-                <div className="container">
-                    <div className='row'>
-                        <div className='col-lg-2'></div>
-                        <div className='col-lg-8'>
-                           
-                            <h4 className='text-center mt-5 font-weight-normal'>Reset Password</h4>
-                            <p className="mt-2 mb-5 text-center  text-secondary ">Please Enter Your New Password</p>
-                            <form action="/action_page.php" className={Forgotpassword.mb}>
-                                <div className="form-group">
-                                    <input type="password" className="form-control py-4" placeholder="Password" id="password" />
-                                </div>
-                                <div className="form-group">
-                                    <input type="password" className="form-control py-4" placeholder="Confirmed Password" id="password" />
-                                </div>
-
-
-                                <button type="submit" className={Forgotpassword.success_btn}>Get New Password</button>
-                                <Link href="/login">
-                                    <button type="submit" className={Forgotpassword.danger_btn}>
-                                        Cancel </button>
-                                </Link>
-                                <Link href="/login">
-                                <div><a href="" className={Forgotpassword.backlink}>Back To Login</a></div>
-                                </Link>
-                            </form>
-
-                        </div>
-                        <div className='col-lg-2'></div>
-                    </div>
+                <div className="form-group">
+                  <input value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}  type="password" className="form-control py-4" placeholder="Confirmed Password" id="password" />
+                  <p style={{ fontStyle: "italic", color: "red" }}>{confirmPasswordError}</p>
                 </div>
+
+
+                <button type="submit" className={Forgotpassword.success_btn}>Get New Password</button>
+                <Link href="/login">
+                  <button className={Forgotpassword.danger_btn}>
+                    Cancel </button>
+                </Link>
+                <Link href="/login">
+                  <div><a href="" className={Forgotpassword.backlink}>Back To Login</a></div>
+                </Link>
+              </form>
+
             </div>
+            <div className='col-lg-2'></div>
+          </div>
+        </div>
+      </div>
 
 
-        </>
-    )
+    </>
+  )
 }
-Forgot.getLayout = function getLayout(page) {
-    return (
-        <Layout>
-            {page}
-        </Layout>
-    )
+Forgot.getLayout = function getLayout ( page ) {
+  return (
+    <Layout>
+      {page}
+    </Layout>
+  )
 }
