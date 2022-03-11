502177de7527bc6ac7c855eaba05a2dc8dea0f5a
Khuram K <khurramk@codeupscale.com>
2022-03-11 17:56:52 +0500

    role base redirection on login

--- pages/dashboard/company/index.js
+++ pages/dashboard/company/index.js
@@ -1,20 +1,31 @@
 import LogoutButton from '../../../components/buttons/Logout';
 import FullLayout from "../../../components/dashboard/layouts/Layout/FullLayout";
 import { Col, Row } from "reactstrap";
+import useAuth from '../../../hooks/useAuth';
+import { useRouter } from 'next/router'
 
 
-  
-  
-  export default function Dashboard() {
+
+
+export default function Dashboard() {
+
+    const { isDriver } = useAuth();
+
+    const router = useRouter()
+
+    if (!isDriver()) {
+        // router.push('/')
+    }
+
     return (
         <>
-         
+
             <div>
                 {/***Top Cards***/}
                 <Row>
-                   <h1>Company Profile Dashboard</h1>
+                    <h1>Company Profile Dashboard</h1>
                 </Row>
-               
+
             </div>
         </>
     )
--- pages/dashboard/driver/index.js
+++ pages/dashboard/driver/index.js
@@ -2,21 +2,28 @@ import LogoutButton from '../../../components/buttons/Logout';
 import FullLayout from "../../../components/dashboard/layouts/FullLayout";
 import { Col, Row } from "reactstrap";
 import useAuth from '../../../hooks/useAuth';
+import { useRouter } from 'next/router'
 
-  
-  
-  export default function Dashboard() {
-    const { authCheck } = useAuth();
-    console.log('Dashboard  authCheck', authCheck())
+
+export default function Dashboard() {
+
+    const { isDriver } = useAuth();
+
+    const router = useRouter()
+
+    if (!isDriver()) {
+        // router.push('/')
+    }
+
     return (
         <>
-         
+
             <div>
                 {/***Top Cards***/}
                 <Row>
-                   <h1>Dashboard</h1>
+                    <h1>Dashboard</h1>
                 </Row>
-               
+
             </div>
         </>
     )
