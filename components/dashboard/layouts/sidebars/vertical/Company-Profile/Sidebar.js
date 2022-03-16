debf37a0f3452995bfda5822225671f99c6ee13e
Muhammad Irfan <irfan@codeupscale.com>
2022-03-16 14:55:35 +0500

    DRIV-101 Add active link in Comapany dashboard Sidebar

--- components/dashboard/layouts/sidebars/vertical/Company-Profile/Sidebar.js
+++ components/dashboard/layouts/sidebars/vertical/Company-Profile/Sidebar.js
@@ -3,108 +3,70 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 
 
-const navigation = [
-  {
-    title: "Dashboard",
-    href: "/dashboard/company",
-    icon: "bi bi-speedometer2",
-    
-  },
-  {
-    title: "Job Listings",
-    href: "/dashboard/company/job-listing",
-    icon: "bi bi-hdd-stack",
-  },
-  {
-    title: "Add new Jobs",
-    href: "/dashboard/company/new-job",
-    icon: "bi bi-card-text",
-  },
-  {
-    title: "Applicants ",
-    href: "/dashboard/company/applicants",
-    icon: "bi bi-columns",
-  },
-  {
-    title: "Send SMS",
-    href: "/dashboard/company/sms",
-    icon: "bi bi-layout-split",
-  },
-  {
-    title: "Driver applications and resume",
-    href: "/dashboard/company/driver-applications-and-resume",
-    icon: "bi bi-layout-split",
-  },
-  {
-    title: "Driver Directory",
-    href: "/dashboard/company/driver-directory",
-    icon: "bi bi-layout-split",
-  },
-  {
-    title: "Safety and Complience",
-    href: "/dashboard/company/safety-and-complience",
-    icon: "bi bi-layout-split",
-  },
-  {
-    title: "Company settings",
-    href: "/dashboard/company/company-settings",
-    icon: "bi bi-layout-split",
-  },
-  {
-    title: "Company Profile",
-    href: "/dashboard/company/company-profile",
-    icon: "bi bi-layout-split",
-  },
-  
-  {
-    title: "Admin approval for jobs",
-    href: "/dashboard/company/admin-approval-jobs",
-    icon: "bi bi-layout-split",
-  },
-
-
-
-
-];
-
 const Sidebar = ({ showMobilemenu }) => {
+
+  const router = useRouter();
   let curl = useRouter();
   const location = curl.pathname;
 
   return (
-   
-      <div className="">
-        <div className="d-flex align-items-center bg_color">
-          <Button
-            close
-            size="sm"
-            className="ms-auto d-lg-none"
-            onClick={showMobilemenu}
-          ></Button>
-        </div>
-        <div className="">
-          <Nav vertical className="sidebarNav">
-            {navigation.map((navi, index) => (
-              <NavItem key={index} className="sidenav-bg">
-                <Link href={navi.href}>
-                  <a
-                    className={
-                      location === navi.href
-                        ? "text-primary nav-link py-3"
-                        : "nav-link text-secondary py-3"
-                    }
-                  >
-                    <i className={navi.icon}></i>
-            
-                    <span className="ms-3 d-inline-block">{navi.title}</span>
-                  </a>
-                </Link>
-              </NavItem>
-            ))}
-          </Nav>
-        </div>
+
+    <div className="">
+      <div className="d-flex align-items-center bg_color">
+        <Button
+          close
+          size="sm"
+          className="ms-auto d-lg-none"
+          onClick={showMobilemenu}
+        ></Button>
       </div>
-   
+      <div className="">
+          <ul className="dashboardsidebar">
+            <li className={router.pathname == "/dashboard/company" ? "active" : ""}>
+              <Link className="sidenav-bg" href="/dashboard/company">Dashboard</Link>
+            </li>
+            <li className={router.pathname == "/dashboard/company/job-listing" ? "active" : ""}>
+              <Link href="/dashboard/company/job-listing">Job Listings</Link>
+
+            </li>
+            <li className={router.pathname == "/dashboard/company/new-job" ? "active" : ""}>
+              <Link href="/dashboard/company/new-job">Add new Jobs</Link>
+            </li>
+            <li className={router.pathname == "/dashboard/company/applicants" ? "active" : ""}>
+              <Link href="/dashboard/company/applicants">Applicants</Link>
+            </li>
+            <li className={router.pathname == "/dashboard/company/sms" ? "active" : ""}>
+              <Link href="/dashboard/company/sms"> Send SMS</Link>
+            </li>
+            <li className={router.pathname == "/dashboard/company/driver-applications-and-resume" ? "active" : ""}>
+              <Link href="/dashboard/company/driver-applications-and-resume"> Driver applications and resume</Link>
+            </li>
+            <li className={router.pathname == "/dashboard/company/driver-directory" ? "active" : ""}>
+              <Link href="/dashboard/company/driver-directory"> Driver Directory</Link>
+            </li>
+            <li className={router.pathname == "/dashboard/company/safety-and-complience" ? "active" : ""}>
+              <Link href="/dashboard/company/safety-and-complience">Safety and Complience</Link>
+            </li>
+            <li className={router.pathname == "/dashboard/company/company-settings" ? "active" : ""}>
+              <Link href="/dashboard/company/company-settings"> Company Settings</Link>
+            </li>
+            <li className={router.pathname == "/dashboard/company/company-profile" ? "active" : ""}>
+              <Link href="/dashboard/company/company-profile">Company Profile</Link>
+            </li>
+            <li className={router.pathname == "/dashboard/company/admin-approval-jobs" ? "active" : ""}>
+              <Link href="/dashboard/company/admin-approval-jobs">Admin approval for jobs</Link>
+            </li>
+
+            
+          </ul>
+       
+        {
+
+        },
+
+      </div>
+    </div>
+
   );
 };
 
