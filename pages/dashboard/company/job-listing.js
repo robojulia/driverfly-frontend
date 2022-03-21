d1d77803bd94a2ad4ff5e4f86bb9833387f913ca
Khuram K <khurramk@codeupscale.com>
2022-03-21 12:34:07 +0500

    m

--- pages/dashboard/company/job-listing.js
+++ pages/dashboard/company/job-listing.js
@@ -14,10 +14,10 @@ import bg4 from "../../../public/dashboard/assets/images/bg/bg4.jpg";
 import JobList from "../../../public/dashboard/styles/css/JobList.module.css"
 import Link from "next/link";
 import useRedirect from '../../../hooks/useRedirect';
-
-
-
-
+import { useEffect } from "react";
+import useAuth from "../../../hooks/useAuth";
+import { useState } from "react";
+import axios from "axios";
 
 export default function JobListing() {
 
@@ -25,118 +25,107 @@ export default function JobListing() {
 
     authCompany()
 
+    const { authCheck } = useAuth();
+
+    const user = authCheck();
+
+    const [jobs, setJobs] = useState([])
+
+    const fetchJobs = () => {
+
+        const headers = {
+            'Authorization': `Bearer ${user.token}`,
+        };
+
+        axios.get(
+            `${process.env.BASE_URL_API}/jobs/`,
+            { headers: headers }
+        )
+            .then(data => {
+                console.log("handle success", data.data)
+                setJobs(data.data)
+            })
+            .catch(function (error) {
+                console.log("handle error success", error.response)
+            }).then(function () {
+                console.log("always executed")
+            })
+    }
+
+    useEffect(() => {
+        fetchJobs()
+    }, []);
+
     return (
         <>
             <div className={JobList.joblisting}>
 
                 <Row className={JobList.link}>
                     <Col sm="6" lg="8"> <h2>Job Listings</h2></Col>
-                    <Col sm="6" lg="4" className={JobList.text_right}>
-                        <Link href="">
-                            <a>Embed job board on your website</a>
-                        </Link>
-                    </Col>
-                    <Col sm="6" lg="8"></Col>
-                    <Col sm="6" lg="4" className={JobList.text_right}>
-                        <Link href="">
-                            <a>Embed job board on your website</a>
-                        </Link>
-                    </Col>
 
                 </Row>
-                <Row>
-                    <Col className="mt-4">
-                        <p className={JobList.fields_show}>
-                            <Link href="">
-                                <a>Edit fields shown</a>
-                            </Link>
-                        </p>
-                    </Col>
-                </Row>
-
-                <Row>
-                    <Col lg="12 p-0">
+                <Row className="mt-5">
+                    <Col lg="12 ">
                         <Card>
                             <CardBody className={JobList.jobtable}>
                                 <div className="table-responsive">
                                     <Table striped>
                                         <thead>
                                             <tr>
-
-                                                <th>Full Name</th>
+                                                <th>Title</th>
                                                 <th>Post Date</th>
-                                                <th>Expiration Date</th>
-                                                <th>Status</th>
+                                                <th>Job Type</th>
                                                 <th>Applied</th>
                                                 <th>Approved</th>
                                                 <th>Hired</th>
                                                 <th className={JobList.display}>
-                                                    <span className={JobList.tag}> Tags </span>
-                                                    <span>
-                                                        <div className="input-group rounded">
-                                                            <input type="search" className="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
-                                                            <span className="input-group-text border-0" id="search-addon">
-                                                                <i className="fas fa-search"></i>
-                                                            </span>
-                                                        </div>
-                                                    </span>
+                                                    <span className={JobList.tag}>  </span>
                                                 </th>
-                                                <th>Outside Jobe Board(?)</th>
 
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            <tr>
-                                                <td>Class A CDL – Texas –NE OTR
-                                                </td>
-                                                <td>November 12, 20</td>
-                                                <td>Dec 2, 2022</td>
-                                                <td>Expired</td>
-                                                <td> <span className={JobList.applied}>6</span></td>
-                                                <td> <span className={JobList.approved}>4</span></td>
-                                                <td> <span className={JobList.hired}>1</span></td>
-                                                <td>
-                                                    <span className={JobList.tags}>OTR</span>
-                                                    <span className={JobList.tags}>1099</span>
-                                                    <input className={JobList.repost}
-                                                        type="button"
-                                                        value="Repost"></input>
-                                                </td>
-                                                <td>
-                                                    <input className={JobList.depolybtn}
-                                                        type="button"
-                                                        value="Depoly"></input>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>Class A CDL – Texas –NE OTR
-                                                </td>
-                                                <td>November 12, 20</td>
-                                                <td>Dec 2, 2022</td>
-                                                <td>Expired</td>
-                                                <td> <span className={JobList.applied}>6</span></td>
-                                                <td> <span className={JobList.approved}>4</span></td>
-                                                <td> <span className={JobList.hired}>1</span></td>
-                                                <td>
-                                                    <span className={JobList.tags}>OTR</span>
-                                                    <span className={JobList.tags}>1099</span>
-                                                    <input className={JobList.repost}
-                                                        type="button"
-                                                        value="Repost"></input>
-                                                </td>
-                                                <td>
-                                                    <input className={JobList.depolybtn}
-                                                        type="button"
-                                                        value="Depoly"></input>
-                                                </td>
-                                            </tr>
+                                            {jobs.length > 0 && jobs.map((job, index) => (
+
+                                                <tr>
+                                                    <td>
+                                                        {job.title}
+                                                    </td>
+                                                    <td>
+                                                        {new Date(job.created_at).toDateString()}
+                                                    </td>
+                                                    <td>
+                                                        {job.job_type}
+                                                    </td>
+                                                    <td>
+                                                        <span className={JobList.applied}>
+                                                            6
+                                                        </span>
+                                                    </td>
+                                                    <td>
+                                                        <span className={JobList.approved}>
+                                                            4
+                                                        </span>
+                                                    </td>
+                                                    <td>
+                                                        <span className={JobList.hired}>
+                                                            1
+                                                        </span>
+                                                    </td>
+                                                    <td>
+                                                        <span className={JobList.tags}>
+                                                            {job.employment_type}
+                                                        </span>
+                                                    </td>
+                                                </tr>
+                                            ))}
 
 
                                         </tbody>
                                     </Table>
                                 </div>
                                 <Row className="py-4">
-                                    <Col sm="6" lg="10">
+                                    {/* <Col sm="6" lg="10">
                                         <nav aria-label="Page navigation example p-0">
                                             <ul className="pagination p-0">
                                                 <li className="page-item"><a className="page-link" href="#">Previous</a></li>
@@ -146,11 +135,13 @@ export default function JobListing() {
                                                 <li className="page-item"><a className="page-link" href="#">Next</a></li>
                                             </ul>
                                         </nav>
-                                    </Col>
+                                    </Col> */}
                                     <Col sm="6" lg="2" >
-                                        <input className={JobList.repost}
-                                            type="button"
-                                            value="+ New Job"></input>
+                                        <Link href="/dashboard/company/new-job">
+                                            <a className={JobList.repost}>
+                                                + New Job
+                                            </a>
+                                        </Link>
                                     </Col>
                                 </Row>
                             </CardBody>
@@ -158,7 +149,7 @@ export default function JobListing() {
                         </Card>
                     </Col>
                 </Row>
-                <div className={JobList.job__overview}>
+                {/* <div className={JobList.job__overview}>
                     <Row>
 
                         <Col className="p-0" lg="2">
@@ -304,7 +295,7 @@ export default function JobListing() {
                                 "></input>
                         </Col>
                     </Row>
-                </div>
+                </div> */}
             </div>
         </>
     )
