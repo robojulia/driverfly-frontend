a8fd1b9f8b06fd07ae9fb58e298dca7a63421cee
Muhammad Irfan <irfan@codeupscale.com>
2022-03-24 13:47:17 +0500

    DRIV-132 Driver Account UI

--- components/dashboard/layouts/sidebars/vertical/Driver-Account/Sidebar.js
+++ components/dashboard/layouts/sidebars/vertical/Driver-Account/Sidebar.js
@@ -33,9 +33,6 @@ export default function Sidebar() {
                 <Link href="/dashboard/driver/jobs-offered">Jobs Offered</Link>
 
               </li>
-              <li className={router.pathname == "/dashboard/driver/application" ? "active" : ""}>
-                <Link href="/dashboard/driver/application">My Application</Link>
-              </li>
               <li className={router.pathname == "/dashboard/driver/driver-intake-information" ? "active" : ""}>
                 <Link href="/dashboard/driver/driver-intake-information">Driver Intake Information</Link>
               </li>
