a1c6df6e0f2ae82074850031b5b58ec34d71ab31
Muhammad Irfan <irfan@codeupscale.com>
2022-03-30 14:27:23 +0500

    DRIV-141 Free Resources Page UI And Hide Sidebar Scroll

--- pages/dashboard/driver/free-resources.js
+++ pages/dashboard/driver/free-resources.js
@@ -21,33 +21,33 @@ export default function FreeResources() {
                 <h1>Free Resources</h1>
             </div>
             <Container fluid>
-                <Row className='mt-3'>
-                    <Col className='col-lg-4 col-12'>
-                        <h2>Vendor Discounts</h2>
+                <Row className='mt-5'>
+                    <Col className='col-lg-4 col-md-4  col-12 my-lg-0 my-4'>
+                        <h2> <Link href="#"><a className={style.link_style}>Vendor Discounts</a></Link> </h2>
                         <h3 className='text-secondary mt-3'>(takes to table list of vendors<br></br> and discount offer codes)</h3>
                     </Col>
-                    <Col className='col-lg-4 col-12'>
-                        <h2>Salary Comparison Tool</h2>
+                    <Col className='col-lg-4  col-md-4 col-12  my-lg-0 my-4'>
+                        <h2> <Link href="#"><a className={style.link_style}>Salary Comparison Tool</a></Link> </h2>
                         <h3 className='text-secondary mt-3'>(redirects to jotform calculator <br></br> on ctrecruiting.com page<br></br> –Julia to give you embeddable code) </h3>
                     </Col>
-                    <Col className='col-lg-4 col-12'>
-                        <h2>Access Your MVR</h2>
-                        <h3 className=' mt-3'>If any past employers have<br></br> pulled your MVR through our<br></br> system, it should be visible to<br></br> you in 
-                        <Link href="#"><a> My Docs</a></Link> 
+                    <Col className='col-lg-4  col-md-4 col-12  my-lg-0 my-4'>
+                        <h2> <Link href="#"><a className={style.link_style}>Access Your MVR</a></Link> </h2>
+                        <h3 className=' mt-3'>If any past employers have<br></br> pulled your MVR through our<br></br> system, it should be visible to <br></br> you in
+                            <Link href="#"><a className={style.link}> My Docs</a></Link>
                         </h3>
                     </Col>
                 </Row>
-                <Row className='mt-5'>
-                    <Col className='col-lg-4 col-12'>
-                        <h2>Refer A Friend Program</h2>
+                <Row className={style.mt_90}>
+                    <Col className='col-lg-4  col-md-4 col-12  my-lg-0 my-4'>
+                        <h2> <Link href="#"><a className={style.link_style}>Refer A Friend Program</a></Link> </h2>
                         <h3 className='text-secondary mt-3'>(takes to referral program like <br></br> on ctrecruiting.com – Julia to <br></br> provide code if needed)</h3>
                     </Col>
-                    <Col className='col-lg-4 col-12'>
-                        <h2>Enter Sweepstakes</h2>
+                    <Col className='col-lg-4  col-md-4 col-12  my-lg-0 my-4'>
+                        <h2> <Link href="#"><a className={style.link_style}>Enter Sweepstakes</a></Link> </h2>
                         <h3 className='text-secondary mt-3'>(redirects to spinner tool—to<br></br>be created at a later time) </h3>
                     </Col>
-                    <Col className='col-lg-4 col-12'>
-                        <h2>Become Your Own Boss</h2>
+                    <Col className='col-lg-4  col-md-4 col-12  my-lg-0 my-4'>
+                        <h2> <Link href="#"><a className={style.link_style}>Become Your Own Boss</a></Link> </h2>
                         <h3 className='text-secondary mt-3'>(redirects to<br></br> drivergrowth.com)</h3>
                     </Col>
                 </Row>
