59836db798302831035de033eb907ec1b51959c1
Khuram K <khurramk@codeupscale.com>
2022-04-01 03:48:00 +0500

    location text field filter

--- components/location/Location.js
+++ components/location/Location.js
@@ -1,12 +1,19 @@
 import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css';
 import React, { useState } from 'react';
 import RangeSlider from 'react-bootstrap-range-slider';
+import { useContext } from "react"
+import jobContext from "../../context/jobContext"
 
 
 export default function Range() {
 
-
     const [value, setValue] = React.useState(50);
+    const { filters, applyFilters } = useContext(jobContext)
+    const keyPressHandler = e => {
+        console.log('e.target.value', e.target.value)
+        filters.location = e.target.value
+        applyFilters()
+    }
 
     return (
         <>
@@ -15,16 +22,21 @@ export default function Range() {
                     <h4 className="clearfix mb-0">
                         <a className="btn-3 btn-link" data-toggle="collapse"
                             data-target="#collapseTwo" aria-expanded="true"
-                            aria-controls="collapseTwo">Location <i
-                                className="fa fa-angle-down"></i></a>
-
-
+                            aria-controls="collapseTwo">
+                            Location
+                            <i className="fa fa-angle-down"></i>
+                        </a>
                     </h4>
                 </div>
                 <div id="collapseTwo" className="collapse show "
                     aria-labelledby="headingTwo" data-parent="#accordionExample">
                     <div className="card-body">
-                        <input type="text" className="form-control p-4"
+                        <input
+                            onKeyUpCapture={e => setTimeout(() => {
+                                keyPressHandler(e)
+                            }, 500)}
+                            type="text"
+                            className="form-control p-4"
                             placeholder="All Location" />
                         <div className='mt-3 text-info'>Radius: {value} miles</div>
                         <RangeSlider
