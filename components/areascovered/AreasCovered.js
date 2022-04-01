a79dd0b26b1fdab95959842b104b2819dac61104
Khuram K <khurramk@codeupscale.com>
2022-04-01 15:36:05 +0500

    areas covered filter

--- components/areascovered/AreasCovered.js
+++ components/areascovered/AreasCovered.js
@@ -2,17 +2,12 @@ import { deleteKey, updateQueryStringParameter } from "../../logics/utils"
 import { useRouter } from "next/router"
 import { useContext } from "react"
 import jobContext from "../../context/jobContext"
-export default function AreasCovered () {
-const router = useRouter()
-const ctx = useContext( jobContext )
-function changeHandler ( e ) {
-  // if its not checked, add it to the query string
-  if (e.target.checked) {
-    const a = updateQueryStringParameter( window.location.href, 'areas_covered[]', e.target.value )
-    router.replace( a )
-    ctx.applyFilters()
-  }
-}
+import { areas_covered } from "../../enums/jobs/job-fields"
+
+export default function AreasCovered() {
+
+  const { state, method } = useContext(jobContext)
+  const { handleChange } = method
 
   return (
     <>
@@ -28,19 +23,27 @@ function changeHandler ( e ) {
         <div id="collapseNine" className="collapse show"
           aria-labelledby="headingNine" data-parent="#accordionExample">
           <div className="card-body">
-            <div onChange={changeHandler} className="App">
-              <div className="topping pt-2">
-                <input type="checkbox" id="Local" name="areas" value="local" /> Local(5)
+            <div className="App">
+              <div class="topping ">
+                <input
+                  onChange={handleChange}
+                  type="radio"
+                  name="areas_covered"
+                  value="" /> Any
               </div>
-              <div className="topping pt-2">
-                <input type="checkbox" id="Regional" name="areas" value="regional" /> Regional(7)
-              </div>
-              <div className="topping pt-2">
-                <input type="checkbox" id="ort" name="areas" value="otr" /> OTR (12)
-              </div>
-              <div className="topping pt-2">
-                <input type="checkbox" id="crossBorder" name="areas" value="crossborder" />  CrossBorder (1)
-              </div>
+              {Object.keys(areas_covered).map((key) => {
+                return (
+                  <>
+                    <div class="topping pt-2">
+                      <input
+                        onChange={handleChange}
+                        type="radio"
+                        name="areas_covered"
+                        value={key} /> {areas_covered[key]}
+                    </div>
+                  </>
+                )
+              })}
             </div>
           </div>
         </div>
