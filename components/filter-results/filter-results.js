ebbdfeaebe9d83981981f26b3f4d4d33dab94469
Khuram K <khurramk@codeupscale.com>
2022-04-02 12:37:05 +0500

    extra filter removed

--- components/filter-results/filter-results.js
+++ components/filter-results/filter-results.js
@@ -43,7 +43,7 @@ export default function FilterResults () {
                 < Salary />
                 < PostedDate />
                 < AreasCovered />
-                < FullParty />
+                {/* < FullParty /> */}
                 < EmploymentType />
                 < TypeOfDelivery />
                 < AcceptingDriversFrom />
