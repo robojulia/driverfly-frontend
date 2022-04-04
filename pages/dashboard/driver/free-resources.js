f265928f10ddd38d93cbfdf31882b3cb0da955e0
Kom <kom@ohmvision.com>
2022-04-04 07:44:51 +0200

    DRIV-168: Updated links in free resources to open in new tab

--- pages/dashboard/driver/free-resources.js
+++ pages/dashboard/driver/free-resources.js
@@ -27,17 +27,17 @@ export default function FreeResources() {
             <Container fluid>
                 <Row className='mt-5'>
                     <Col className='col-lg-4 col-md-4  col-12 my-lg-0 my-4'>
-                        <h2> <Link href="/resources"><a className={style.link_style}>{t("vendor_discounts")}</a></Link> </h2>
+                        <h2> <Link href="/resources" target="_blank"><a className={style.link_style}>{t("vendor_discounts")}</a></Link> </h2>
                         <br />
                         <span className={style.description}>{t("vendor_discounts_paragraph")}</span>
                     </Col>
                     <Col className='col-lg-4  col-md-4 col-12  my-lg-0 my-4'>
-                        <h2> <Link href="https://ctrecruiting.com/pay-calculator"><a className={style.link_style}>{t("salary_comparison_tool")}</a></Link> </h2>
+                        <h2> <Link href="https://ctrecruiting.com/pay-calculator" target="_blank"><a className={style.link_style}>{t("salary_comparison_tool")}</a></Link> </h2>
                         <br />
                         <span className={style.description}>{t("salary_comparison_tool_paragraph")}</span>
                     </Col>
                     <Col className='col-lg-4  col-md-4 col-12  my-lg-0 my-4'>
-                        <h2> <Link href="/dashboard/driver/my-docs"><a className={style.link_style}>{t("access_your_mvr")}</a></Link> </h2>
+                        <h2> <Link href="/dashboard/driver/my-docs" target="_blank"><a className={style.link_style}>{t("access_your_mvr")}</a></Link> </h2>
                         <br />
                         <span className={style.description}>{t("access_your_mvr_paragraph")}</span>
                     </Col>
@@ -54,7 +54,7 @@ export default function FreeResources() {
                         <span className={style.description}>{t("enter_sweepstakes_paragraph")}</span>
                     </Col>
                     <Col className='col-lg-4  col-md-4 col-12  my-lg-0 my-4'>
-                        <h2> <Link href="https://drivergrowth.com/"><a className={style.link_style}>{t("become_your_own_boss")}</a></Link> </h2>
+                        <h2> <Link href="https://drivergrowth.com/" target="_blank"><a className={style.link_style}>{t("become_your_own_boss")}</a></Link> </h2>
                         <br />
                         <span className={style.description}>{t("become_your_own_boss_paragraph")}</span>
                     </Col>
@@ -72,21 +72,21 @@ export default function FreeResources() {
                             <span className={style.description}>{t("follow_us_paragraph")}</span>
                             <br />
                             <br />
-                            <Link href="https://www.facebook.com/DriverFlyJobs/">
+                            <Link href="https://www.facebook.com/DriverFlyJobs/" target="_blank">
                                 <a target="_blank">
                                    <div className={style.social_icon}> <Image src={fb} alt="fb" /> </div>
                                    facebook.com/DriverFlyJobs
                                 </a>
                             </Link>
                             <br />
-                            <Link href="https://www.instagram.com/driver_hiring" >
+                            <Link href="https://www.instagram.com/driver_hiring" target="_blank" >
                                 <a target="_blank">
                                    <div className={style.social_icon}> <Image src={insta} alt="ig" /> </div>
                                    @driver_hiring
                                 </a>
                             </Link>
                             <br />
-                            <Link href="https://www.linkedin.com/company/driverfly/">
+                            <Link href="https://www.linkedin.com/company/driverfly/" target="_blank">
                                 <a target="_blank">
                                    <div className={style.social_icon}> <Image src={linkedin} alt="linkedin" />  </div>
                                    linkedin.com/company/driverfly
