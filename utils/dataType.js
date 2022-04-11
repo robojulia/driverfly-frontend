7c88bc22ec323d4ec6544bae23a4dd3ba918cd5f
Kom <kom@ohmvision.com>
2022-04-11 18:07:13 +0200

    updated create job form to force data types

---
+++ utils/dataType.js
@@ -0,0 +1,7 @@
+function forceInt(value) {
+    return typeof value === "string" ? parseInt(value) : value;
+}
+
+function forceCurrency(value) {
+    return typeof value === "string" ? parseFloat(value) : value;
+}
