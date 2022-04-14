fa675defa4101f634026b3c8dab59ddfb92406ad
Muhammad Irfan <irfan@codeupscale.com>
2022-04-14 16:16:10 +0500

    DRIV-248 Fix Dashboard Spelling and Add text in translation

--- components/buttons/Signup.js
+++ components/buttons/Signup.js
@@ -1,10 +1,13 @@
 import Link from "next/link";
+import { useTranslation } from "react-i18next";
+
 
 export default function Signup(props) {
+    const { t } = useTranslation();
     return (
         <>
             <Link href="/signup">
-                <button type="button" className={props.className}>Sign Up</button>
+                <button type="button" className={props.className}>{t("SIGN_UP")}</button>
             </Link>
         </>
     )
