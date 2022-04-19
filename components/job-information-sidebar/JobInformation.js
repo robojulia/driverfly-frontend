a9d24f581e9de35420ee22e3daf63ccb46c2c9d6
Khuram K <khurramk@codeupscale.com>
2022-04-19 17:01:51 +0500

    m

--- components/job-information-sidebar/JobInformation.js
+++ components/job-information-sidebar/JobInformation.js
@@ -7,6 +7,7 @@ import { JobSchedule } from "../../enums/jobs/job-schedule.enum";
 import { JobPayMethod } from "../../enums/jobs/job-pay-method.enum";
 import { MvrType } from "../../enums/drivers/mvr-type.enum";
 import timeSince from "../../utils/timeSince";
+import ShowEnumFromString from "../../components/enum-filters/show-enum-from-string"
 
 export default function JonInformation({ job }) {
 
