c40a5547ea7696504b751d7c482fbc7d95c71570
Khuram K <khurramk@codeupscale.com>
2022-04-28 07:17:40 +0500

    company image for job

--- components/jobs/company-photo.js
+++ components/jobs/company-photo.js
@@ -4,15 +4,13 @@ import DocumentApi from "../../pages/api/document";
 export default function CompanyPhoto(props) {
 
     const [photo, setPhoto] = useState("/driverfly-logo-square.png")
-    // console.log("props.company", props.company);
 
     useEffect(async () => {
         const api = new DocumentApi();
-        if (props.company) {
-            await api.getCompanyPhoto(props.company.id)
-                .then(base64 => {
-                    // console.log("base64", base64);
-                    setPhoto(base64 || "/driverfly-logo-square.png")
+        if (props.company?.photo) {
+            await api.getSignedUrl(props.company.photo.id)
+                .then(file => {
+                    setPhoto(file.path || "/driverfly-logo-square.png")
                 }).catch(error => {
                     console.error("error", error);
                 })
