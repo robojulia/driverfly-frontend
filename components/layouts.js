ba2f750eacf7d30ddb0865b3922719bf46fba1e1
Khuram K <khurramk@codeupscale.com>
2022-05-09 15:01:14 +0500

    cdn removed

--- components/layouts.js
+++ components/layouts.js
@@ -10,8 +10,6 @@ export default function Layout({ children }) {
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
                 <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-                <link href="https://fonts.cdnfonts.com/css/arial" rel="stylesheet" />
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
             </Head>
             <Header />
             <main>{children}</main>
