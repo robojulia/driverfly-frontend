9d7a9415c22ee838a616e4a17959c5a934978e24
Khuram K <khurramk@codeupscale.com>
2022-05-11 12:51:25 +0500

    changes

--- components/apply.js
+++ components/apply.js
@@ -260,7 +260,7 @@ export default function JobApply({ job }) {
   return (
     <>
 
-      <div className="modal fade p-0" id="exampleModal"  role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div className="modal fade p-0" id="exampleModal" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div className="modal-dialog" role="document">
           <div className="modal-content">
             <form onSubmit={apply_form.handleSubmit}>
@@ -359,6 +359,18 @@ export default function JobApply({ job }) {
                       name={JobApplicantDocumentType.DRIVER_LICENSE}
                       onChange={handleFileChange}
                     />
+                    {
+                      user &&
+                      apply_form.values[JobApplicantDocumentType.DRIVER_LICENSE + "_old"] &&
+                      <span className='col-12'>
+                        {/* {console.log("aasasasas", apply_form.values[JobApplicantDocumentType.DRIVER_LICENSE + "_old"])} */}
+                        <ViewFileButton
+                          data_name={JobApplicantDocumentType.DRIVER_LICENSE + "_old"}
+                          onClick={viewHandler}
+                          label={t("VIEW_{name}", { name: t('OLD_DOCUMENT') })}
+                          className='btn btn-link' />
+                      </span>
+                    }
                   </Col>
                   <Col lg={6}>
                     <BaseInput
@@ -369,6 +381,17 @@ export default function JobApply({ job }) {
                       name={JobApplicantDocumentType.MEDICAL_CARD}
                       onChange={handleFileChange}
                     />
+                    {
+                      user &&
+                      apply_form.values[JobApplicantDocumentType.MEDICAL_CARD + "_old"] &&
+                      <span className='col-12'>
+                        <ViewFileButton
+                          data_name={JobApplicantDocumentType.MEDICAL_CARD + "_old"}
+                          onClick={viewHandler}
+                          label={t("VIEW_{name}", { name: t('OLD_DOCUMENT') })}
+                          className='btn btn-link' />
+                      </span>
+                    }
                   </Col>
                 </Row>
                 <Row>
@@ -381,6 +404,17 @@ export default function JobApply({ job }) {
                       name={JobApplicantDocumentType.RESUME}
                       onChange={handleFileChange}
                     />
+                    {
+                      user &&
+                      apply_form.values[JobApplicantDocumentType.RESUME + "_old"] &&
+                      <span className='col-12'>
+                        <ViewFileButton
+                          data_name={JobApplicantDocumentType.RESUME + "_old"}
+                          onClick={viewHandler}
+                          label={t("VIEW_{name}", { name: t('OLD_DOCUMENT') })}
+                          className='btn btn-link' />
+                      </span>
+                    }
                   </Col>
                   <Col lg={6}>
                     <BaseInput
@@ -391,6 +425,17 @@ export default function JobApply({ job }) {
                       name={JobApplicantDocumentType.MVR}
                       onChange={handleFileChange}
                     />
+                    {
+                      user &&
+                      apply_form.values[JobApplicantDocumentType.MVR + "_old"] &&
+                      <span className='col-12'>
+                        <ViewFileButton
+                          data_name={JobApplicantDocumentType.MVR + "_old"}
+                          onClick={viewHandler}
+                          label={t("VIEW_{name}", { name: t('OLD_DOCUMENT') })}
+                          className='btn btn-link' />
+                      </span>
+                    }
                   </Col>
                 </Row>
                 {/* Files End */}
--- pages/jobs/[id].js
+++ pages/jobs/[id].js
@@ -14,6 +14,7 @@ import { useTranslation } from "../../hooks/useTranslation"
 import JobApi from "../api/job"
 import CompanyPhoto from "../../components/jobs/company-photo"
 
+import StructuredData from "../../components/seo/StructuredData"
 
 export default function Detail({ jobDetail, relatedJobs }) {
 
@@ -23,6 +24,7 @@ export default function Detail({ jobDetail, relatedJobs }) {
   return (
     <>
       <ToastContainer />
+      {/* <StructuredData type="JobPosting" data={StructuredData.JobPosting(jobDetail, t)} /> */}
       <section className="top-links-sec ort-general">
         <div className="container">
           <div className="row">
