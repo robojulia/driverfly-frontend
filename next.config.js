87a5624607b49878f79381f5310f717f8c613033
Adnan Mumtaz <adnanmumtazmayo@gmail.com>
2022-05-27 13:55:19 +0500

    fixed next.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // reactStrictMode: true,
-  reloadOnPrerender: true,
+  // reloadOnPrerender: true,
   env: {
     BASE_URL: process.env.BASE_URL,
     BASE_URL_API: process.env.BASE_URL_API,
