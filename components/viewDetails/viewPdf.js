4da1c8256cfbded238caee7d19ce4a511236c0a2
Adnan Mumtaz <adnanmumtazmayo@gmail.com>
2022-05-30 16:13:18 +0500

    fixed errors

--- components/viewDetails/viewPdf.js
+++ components/viewDetails/viewPdf.js
@@ -1,8 +1,8 @@
 import { Modal, Button, Spinner } from "react-bootstrap";
-import { SpecialZoomLevel, Viewer, Worker } from '@react-pdf-viewer/core'
-import '@react-pdf-viewer/core/lib/styles/index.css'
-import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'
-import '@react-pdf-viewer/default-layout/lib/styles/index.css'
+// import { SpecialZoomLevel, Viewer, Worker } from '@react-pdf-viewer/core'
+// import '@react-pdf-viewer/core/lib/styles/index.css'
+// import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'
+// import '@react-pdf-viewer/default-layout/lib/styles/index.css'
 
 import ViewModal from "./viewModal";
 
