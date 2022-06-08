236a7acf24695bd278ead2fd32a133a8b0633563
Khuram K <khurramk@codeupscale.com>
2022-06-08 16:58:16 +0500

    refactoring

--- components/filters/equipment.js
+++ components/filters/equipment.js
@@ -1,10 +1,12 @@
 import FindJobFilterAccordion from "../find-jobs-accordion/find-job-filter-accordion"
 import ViewMoreRadioFilter from "./view-more-radio-filter";
 import { JobEquipmentType } from '../../enums/jobs/job-equipment-type.enum';
+import { useTranslation } from "../../hooks/useTranslation";
 
 export default function Equipment(props) {
 
-  const { t, state, method } = props
+  const { t } = useTranslation();
+  const { state, method } = props
   const { handleChange } = method
 
   return (
