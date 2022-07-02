d491ac42f20a7107fbbcae2dd4abad5e6e9dc90d
Khuram K <khurramk@codeupscale.com>
2022-07-02 15:06:55 +0500

    DRIV-321 | Company | In browser calling | calling applicant fron list only

--- pages/dashboard/company/call/index.tsx
+++ pages/dashboard/company/call/index.tsx
@@ -6,10 +6,12 @@ import CallApi from "../../../api/call"
 import ApplicantApi from "../../../api/applicant"
 import { ApplicantEntity } from "../../../../models/applicant/applicant.entity";
 import ViewDataTable from "../../../../components/viewDetails/viewDataTable";
-import ShowEnumFromString from "../../../../components/enum-filters/show-enum-from-string";
 import { useEffectAsync } from "../../../../utils/react";
-import { TelephoneFill } from "react-bootstrap-icons";
+import { TelephoneFill, TelephoneMinusFill, TelephoneOutboundFill, TelephonePlus, TelephonePlusFill, XCircleFill } from "react-bootstrap-icons";
 import { useTranslation } from "../../../../hooks/useTranslation";
+import { Container, Modal } from "react-bootstrap";
+import { toast } from 'react-toastify'
+import Spinner from 'react-bootstrap/Spinner'
 
 export default function Call() {
 
@@ -18,142 +20,111 @@ export default function Call() {
     const { t } = useTranslation()
 
     const applicantApi = new ApplicantApi()
+    const callApi = new CallApi()
+
     const [applicants, setApplicants] = useState<ApplicantEntity[]>([])
-
-
     const [device, setDevice] = useState(null)
-    const [identity, setIdentity] = useState('+923214604331')
+    const [identity, setIdentity] = useState<ApplicantEntity>({})
     const [status, setStatus] = useState("Disconnected")
     const [ready, setReady] = useState(false)
+    const [connected, setConnected] = useState(false)
+    const [loading, setloading] = useState(false)
 
-    const connect = () => {
-        const params = {
-            // 'To': identity,
-            'PhoneNumber': identity,
-            // 'call_type': 'voice',
-            'call_mode': 'WEB',
-        };
-
-        try {
-            device.connect(params)
-        } catch (error) {
-            console.error("connection error", error.message)
-        }
+    const [showCaller, setShowCaller] = useState(false)
+    const handleShowCaller = () => setShowCaller(true)
+    const handleHideCaller = () => {
+        disconnectCall()
+        setShowCaller(false)
+        setReady(false);
     }
 
-    const disconnect = () => { device.disconnectAll() }
+    useEffectAsync(async () => {
+        await applicantApi.list()
+            .then(data => setApplicants(data))
+            .catch(error => console.error("applicantApi.list error....", error));
+    }, [])
 
-    const createDevice = async (token) => {
-        console.log("token", token);
-
-        const Device = (await import('twilio-client')).Device
-        const deviceClient = new Device(token, { debug: true, appName: 'driverfly' })
-
-        deviceClient.on("ready", (device) => {
-            console.log("Twilio.Device Ready!");
-            setStatus("Device ready");
-            setReady(true);
-        });
-
-        deviceClient.on("error", (error) => {
-            console.log("Twilio.Device Error: " + error.message);
-            setStatus("ERROR: " + error.message);
-        });
-
-        deviceClient.on("connect", (conn) => {
-            console.log("Successfully established call!");
-
-            // If phoneNumber is part of the connection, this is a call from a
-            // support agent to a customer's phone
-            if ("phoneNumber" in conn.message) {
-                setStatus("In call with " + conn.message.phoneNumber);
-            } else {
-                // This is a call from a website user to a support agent
-                setStatus("In call with support");
-            }
-        });
-
-        deviceClient.on("disconnect", (conn) => {
-            // Disable the hangup button and enable the call buttons
-
-            setStatus("Ready");
-        });
-
-        deviceClient.on("incoming", (conn) => {
-            setStatus("Incoming support call");
-
-            // Set a callback to be executed when the connection is accepted
-            conn.accept(function () {
-                setStatus("In call with customer");
-            });
-
-            // Set a callback on the answer button and enable it
-            // answerButton.click(function () {
-            //     conn.accept();
-            // });
-            // answerButton.prop("disabled", false);
-        });
-
-        setDevice(deviceClient);
-
-    }
-
-    const handleTokenGenerate = async () => {
-        const callApi = new CallApi()
+    const setupTwilio = async () => {
         callApi.generateToken()
             .then(async ({ token }) => {
-                await createDevice(token)
+                await setupClient(token)
                     .catch(error => console.error("creating device error....", error))
             })
     }
 
-    const openCallDialog = (phoneNumber) => { }
+    const setupClient = async (token) => {
+        try {
+            const Device = (await import('twilio-client')).Device
+            const client = new Device(token, { debug: true, appName: process.env.TWILIO_APP_NAME || 'driverfly' })
+
+            client.on("ready", (device) => {
+                setConnected(false)
+                setStatus("Ready to call");
+                setReady(true);
+            });
+
+            client.on("error", (error) => {
+                console.log("Twilio.Device Error: " + error.message);
+                setStatus("ERROR: " + error.message);
+            });
+
+            client.on("connect", (conn) => {
+                setConnected(true)
+                setStatus("Calling")
+                // if ("PhoneNumber" in conn.message) {
+                //     setStatus("In call with " + conn.message.PhoneNumber);
+                // }
+            });
+
+            client.on("disconnect", (conn) => {
+                setConnected(false)
+                setStatus("Ready to call");
+            });
+
+            client.on("incoming", (conn) => {
+                setStatus("Incoming support call");
+
+                conn.accept(function () {
+                    setStatus("In call with user");
+                });
+            });
+
+            setDevice(client);
+        } catch (error) {
+
+        }
+    }
+
+    const connectCall = () => {
+        setConnected(true)
+        if (identity?.phone) device.connect({ 'PhoneNumber': identity.phone, 'call_mode': 'WEB', })
+    }
+
+    const disconnectCall = () => {
+        setConnected(false)
+        device.disconnectAll()
+    }
+
+    const openCallDialog = async (applicant) => {
+        setloading(true)
+        await setupTwilio()
+        setIdentity(applicant)
+    }
 
     useEffectAsync(async () => {
-        await applicantApi.list()
-            .then(data => { console.log("applicantApi", data); setApplicants(data); })
-            .catch(error => console.error("applicantApi.list error....", error))
-
-    }, [])
+        if (!!ready) {
+            handleShowCaller()
+            setloading(false)
+        } else {
+            toast.error(t('COULD_NOT_MAKE_CALL'))
+        }
+    }, [ready])
 
     return (
         <>
-            <button
-                onClick={handleTokenGenerate}
-                className='btn btn-primary m-2'>
-                Connect
-            </button>
-            <div>
-                <h4>Status: {status}</h4>
-                <h4>Number: {identity}</h4>
-                {
-                    ready &&
-                    <>
-                        <input
-                            className='form-control m-2'
-                            value={identity}
-                            onChange={(e) => setIdentity(e.target.value)}
-                            type="text"
-                            placeholder="Enter Number" />
+            {!!loading && <Spinner animation="grow" variant="info" />}
 
-                        <button
-                            className='btn btn-primary m-2'
-                            onClick={connect}
-                        >
-                            Press to Talk
-                        </button>
-                        <button
-                            className='btn btn-primary m-2'
-                            onClick={disconnect}
-                        >
-                            Press to End
-                        </button>
-                    </>
-                }
-            </div>
-
-
-            <Row className="mt-5">
+            <Row className="">
                 <Col lg="12 ">
                     <ViewDataTable
                         columns={[
@@ -173,7 +144,7 @@ export default function Call() {
                         ]}
                         actions={applicant => ([
                             {
-                                onClick: e => openCallDialog(applicant.phone),
+                                onClick: e => openCallDialog(applicant),
                                 label: (<><TelephoneFill />  {t("MAKE_CALL")}</>)
                             },
                         ])}
@@ -182,6 +153,53 @@ export default function Call() {
                 </Col>
             </Row>
 
+            <Modal
+                show={showCaller}
+                onHide={handleHideCaller}
+                backdrop="static"
+                keyboard={false}
+                className="bg-secondary "
+            >
+                <Modal.Header className="dialer__close bg-dark" closeVariant="white" closeButton />
+                <Modal.Body className="bg-dark ">
+                    <Container className="mt-2">
+                        <Row className="justify-content-center">
+                            <div className="phone">
+                                <Col lg='12' className="text-center">
+                                    <div id="btns" className="justify-content-center mt-4">
+                                        <img style={{ width: 300, padding: 30, borderRadius: 250 }} src="/img/avatar.png" />
+                                    </div>
+                                    <h4 className="text-white-50 pb-0">
+                                        {`${identity.first_name} ${identity.last_name}`}
+                                    </h4>
+                                    <h3 className="text-info pb-3">
+                                        {identity.phone}
+                                    </h3>
+                                </Col>
+                                <Col lg='12' className="text-center pb-5  rounded">
+                                    <h5 className="text-white pt-3 pb-2">
+                                        {status}
+                                    </h5>
+                                    {
+                                        !!!connected ?
+                                            <button onClick={connectCall}
+                                                id="call"
+                                                className="btn btn-success rounded-circle">
+                                                < TelephoneOutboundFill />
+                                            </button>
+                                            :
+                                            <button onClick={disconnectCall}
+                                                id="end"
+                                                className="btn btn-danger rounded-circle">
+                                                < TelephoneMinusFill />
+                                            </button>
+                                    }
+                                </Col>
+                            </div>
+                        </Row>
+                    </Container>
+                </Modal.Body>
+            </Modal>
         </>
     )
 };
