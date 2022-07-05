9777c2de5be7a2dc75b8765a84ebbbfb7ee9b618
Khuram K <khurramk@codeupscale.com>
2022-07-05 20:19:46 +0500

    DRIV-541 | sitemap

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,61 @@
+import fs from "fs";
+import JobApi from "./api/job";
+
+const Sitemap = () => { };
+
+export const getServerSideProps = async ({ res }) => {
+
+    const baseUrl = process.env.FRONTEND_BASE_URL;
+    const jobApi = new JobApi()
+    const jobs = await jobApi.sitemap()
+
+    const staticPages = fs
+        .readdirSync("pages")
+        .filter((staticPage) => {
+            return ![
+                "_app.js",
+                "_document.js",
+                "_error.js",
+                "_middleware.ts",
+                "api",
+                "sitemap.xml.js",
+            ].includes(staticPage);
+        })
+        .map((staticPagePath) => {
+            return `${baseUrl}/${staticPagePath.replace('.js', '')}`;
+        });
+
+
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${staticPages.map((url) => (`
+            <url>
+              <loc>${url}</loc>
+              <lastmod>${new Date().toISOString()}</lastmod> 
+              <changefreq>weekly</changefreq> 
+              <priority>1.0</priority>
+            </url>
+          `))
+            .join("")}
+      ${jobs.map(({ id, slug, last_updated_at }) => (`
+            <url>
+              <loc>${baseUrl}/jobs/${id}/${slug}</loc>
+              <lastmod>${last_updated_at}</lastmod>
+              <changefreq>weekly</changefreq>
+              <priority>1.0</priority>
+            </url>
+          `))
+            .join("")}
+    </urlset>
+  `;
+
+    res.setHeader("Content-Type", "text/xml");
+    res.write(sitemap);
+    res.end();
+
+    return {
+        props: {},
+    };
+};
+
+export default Sitemap;
