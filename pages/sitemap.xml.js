a2d3fdd4405d9dc1b4c890df147b3b6875b9f502
Khuram K <khurramk@codeupscale.com>
2022-07-08 14:12:22 +0500

    m

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -11,45 +11,46 @@ export const getServerSideProps = async ({ res }) => {
         const jobApi = new JobApi()
         const jobs = await jobApi.sitemap()
 
-        const staticPages = fs
-            .readdirSync(process.cwd() + "/pages")
-            .filter((staticPage) => {
-                return ![
-                    "_app.js",
-                    "_document.js",
-                    "_error.js",
-                    "_middleware.ts",
-                    "api",
-                    "sitemap.xml.js",
-                ].includes(staticPage);
-            })
-            .map((staticPagePath) => {
-                return `${baseUrl}/${staticPagePath.replace('.js', '')}`;
-            });
+        const sitemap = process.cwd() + "/pages"
+        // const staticPages = fs
+        //     .readdirSync(process.cwd() + "/pages")
+        //     .filter((staticPage) => {
+        //         return ![
+        //             "_app.js",
+        //             "_document.js",
+        //             "_error.js",
+        //             "_middleware.ts",
+        //             "api",
+        //             "sitemap.xml.js",
+        //         ].includes(staticPage);
+        //     })
+        //     .map((staticPagePath) => {
+        //         return `${baseUrl}/${staticPagePath.replace('.js', '')}`;
+        //     });
 
 
-        const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${staticPages.map((url) => (`
-            <url>
-              <loc>${url}</loc>
-              <lastmod>${new Date().toISOString()}</lastmod> 
-              <changefreq>weekly</changefreq> 
-              <priority>1.0</priority>
-            </url>
-          `))
-                .join("")}
-      ${jobs.map(({ id, slug, last_updated_at }) => (`
-            <url>
-              <loc>${baseUrl}/jobs/${id}/${slug}</loc>
-              <lastmod>${last_updated_at}</lastmod>
-              <changefreq>weekly</changefreq>
-              <priority>1.0</priority>
-            </url>
-          `))
-                .join("")}
-    </urlset>
-  `;
+        //         const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+        //     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        //       ${staticPages.map((url) => (`
+        //             <url>
+        //               <loc>${url}</loc>
+        //               <lastmod>${new Date().toISOString()}</lastmod> 
+        //               <changefreq>weekly</changefreq> 
+        //               <priority>1.0</priority>
+        //             </url>
+        //           `))
+        //                 .join("")}
+        //       ${jobs.map(({ id, slug, last_updated_at }) => (`
+        //             <url>
+        //               <loc>${baseUrl}/jobs/${id}/${slug}</loc>
+        //               <lastmod>${last_updated_at}</lastmod>
+        //               <changefreq>weekly</changefreq>
+        //               <priority>1.0</priority>
+        //             </url>
+        //           `))
+        //                 .join("")}
+        //     </urlset>
+        //   `;
 
         res.setHeader("Content-Type", "text/xml");
         res.write(sitemap);
