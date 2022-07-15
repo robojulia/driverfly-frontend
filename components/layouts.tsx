d0268870db10a8562a516a317db98a7e73bbe76b
Muhammad-irfan12345 <irfandhudi.vu@gmail.com>
2022-07-15 15:24:49 +0500

    khurram google job indexing

--- components/layouts.tsx
+++ components/layouts.tsx
@@ -15,6 +15,7 @@ export default function Layout({ children, title }: LayoutProps) {
     return (
         <>
             <Head>
+                <meta name="google-site-verification" content="m9bfVuOGxtYDxi8eKLetXlJplLbdwnewUO37wDyw96I" />
                 <title>{t("Driverfly")} {title ? `| ${t(title)}` : ""}</title>
                 <link rel="icon" href="/img/DriverFly-Official-Favicon.png" />
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
