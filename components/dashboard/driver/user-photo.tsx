cd99f45932d0fbdcddedd2269ea7c5f080ac910c
Muhammad-irfan12345 <irfandhudi.vu@gmail.com>
2022-07-20 19:28:27 +0500

    DRIV-716 [Phase 2] Driver should able to change profile picture

---
+++ components/dashboard/driver/user-photo.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import DocumentApi from "../../../pages/api/document";
+import { useEffectAsync } from "../../../utils/react";
+
+export default function UserPhoto({ user, style, className, width, height }) {
+
+    if (!!!user || !!!user.photo?.id)
+        return <></>
+
+    const [photo, setPhoto] = useState("/dashboard/assets/images/users/user1.jpg")
+    const documentApi = new DocumentApi();
+
+    const fetchUserphoto = async () => {
+        await documentApi.getPhoto(user.photo.id)
+            .then(file => setPhoto(file.path))
+            .catch(error => console.error("error", error))
+    }
+
+    useEffectAsync(async () => {
+        await fetchUserphoto()
+    }, [])
+
+    return <>
+        <img
+            style={style}
+            className={className}
+            width={width}
+            height={height}
+            src={photo} />
+    </>
+}
