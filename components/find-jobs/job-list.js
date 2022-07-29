361abefae6ae985cfbbe7a5ca7029f0071b51d63
Muhammad-irfan12345 <irfandhudi.vu@gmail.com>
2022-07-29 13:43:22 +0500

    DRIV-741 PHASE 2 | Driver Portal | Mobile Responsiveness

--- components/find-jobs/job-list.js
+++ components/find-jobs/job-list.js
@@ -17,68 +17,71 @@ export default function JobsList() {
 
     return (
         <>
-            <div className="filter-outer mt-5">
-                {jobs.length > 0 && jobs.map(job => (
-                    <div key={job.id} className="media align-items-center shadow-sm">
+            <div className='filter-sec'>
+                <div className="filter-outer mt-5">
+                    {jobs.length > 0 && jobs.map(job => (
+                        <div key={job.id} className="media align-items-center shadow-sm">
 
-                        <CompanyPhoto className="d-flex mr-4 truck-img" company={job.company} />
-                        <div className="media-body">
-                            <Link href={`/dashboard/driver/jobs/${job.id}`}>
-                                <a className='text-decoration-none '>
-                                    <h4 className="mt-0">
-                                        {job.title}
-                                    </h4>
-                                </a>
-                            </Link>
-                            <div className="job-date-author">
-                                {
-                                    job.created_at &&
-                                    <>
-                                        {t('posted')} {timeSince(job.created_at)} {t('ago')}
-                                    </>
-                                } {
-                                    job?.company?.name &&
-                                    <>
-                                        {t('by')} <span role="button" className="employer text-theme">{job.company?.name}</span>
-                                    </>
-                                }
-                            </div>
-                            <div className="job-metas text-secondary text-secondary">
-                                <div className="job-location">
+                            <CompanyPhoto className="d-flex mr-4 truck-img" company={job.company} />
+                            <div className="media-body">
+                                <Link href={`/dashboard/driver/jobs/${job.id}`}>
+                                    <a className='text-decoration-none '>
+                                        <h4 className="mt-0">
+                                            {job.title}
+                                        </h4>
+                                    </a>
+                                </Link>
+                                <div className="job-date-author">
                                     {
-                                        job.location &&
+                                        job.created_at &&
                                         <>
-                                           
-                                                {/* < GeoAltFill className='mr-1' /> */}
-                                                <span className='mr-4'>
-                                                {buildAddress(job.location || {},{ street: false, zip_code: false})}
-
-                                                </span>
+                                            {t('posted')} {timeSince(job.created_at)} {t('ago')}
+                                        </>
+                                    } {
+                                        job?.company?.name &&
+                                        <>
+                                            {t('by')} <span role="button" className="employer text-theme">{job.company?.name}</span>
                                         </>
                                     }
-                                    <p>
-                                        < CurrencyDollar className='mr-1' />{job.min_weekly_pay ? job.min_weekly_pay : 0} - {job.max_weekly_pay ? job.max_weekly_pay : 0} {t("per_week")} </p>
                                 </div>
-                                <div className="job-location">
+                                <div className="job-metas text-secondary text-secondary">
+                                    <div className="job-location">
+                                        {
+                                            job.location &&
+                                            <>
 
-                                    <strong className="text-secondary">{job.description_short}</strong>
+                                                {/* < GeoAltFill className='mr-1' /> */}
+                                                <span className='mr-4'>
+                                                    {buildAddress(job.location || {}, { street: false, zip_code: false })}
+
+                                                </span>
+                                            </>
+                                        }
+                                        <p>
+                                            < CurrencyDollar className='mr-1' />{job.min_weekly_pay ? job.min_weekly_pay : 0} - {job.max_weekly_pay ? job.max_weekly_pay : 0} {t("per_week")} </p>
+                                    </div>
+                                    <div className="job-location">
+
+                                        <strong className="text-secondary">{job.description_short}</strong>
+                                    </div>
                                 </div>
                             </div>
+                            <Link href={`/dashboard/driver/jobs/${job.id}`}>
+                                <button type="button" className="theme-primary-btn-outline">{t('view_job')}</button>
+                            </Link>
+
                         </div>
-                        <Link href={`/dashboard/driver/jobs/${job.id}`}>
-                            <button type="button" className="theme-primary-btn-outline">{t('view_job')}</button>
-                        </Link>
+                    ))}
 
+                    <Pagination />
+                    <div className="jumbotron my-4">
+                        <p className="lead">
+                            {t('CANT_FIND_WHAT_YOU_WANT')}
+                        </p>
                     </div>
-                ))}
-
-                <Pagination />
-                <div className="jumbotron my-4">
-                    <p className="lead">
-                        {t('CANT_FIND_WHAT_YOU_WANT')}
-                    </p>
                 </div>
             </div>
+
 
         </>
     )
