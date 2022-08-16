e1cdd88c5d0c358c02277ab2100ac70a463afe0c
Muhammad-irfan12345 <irfandhudi.vu@gmail.com>
2022-08-16 13:25:45 +0500

    DRIV-816 PHASE 1 Feedback | Driver Portal | Job Detail Page

--- components/job-description/JobDescription.js
+++ components/job-description/JobDescription.js
@@ -9,39 +9,43 @@ export default function JobDescription({ job }) {
     return (
         <>
             <div className="job-deatails-inner">
-                <h3>{t('job_description')}</h3>
-                <p>{job.description_short}</p>
-                <p>{job.description}</p>
-
-                <h3 className="mt-5">{t('BENEFITS')}</h3>
-                <p>
-                    <ShowEnumFromString labelPrefix="JobBenefits" str={job.benefits} enumArray={JobBenefits} />
-                    , {job.benefits_other}
-                </p>
-
-                <h3 className="mt-5">{t('REQUIRED_SKILLS')}</h3>
-                <ul>
-                    {job.required_skills &&
-                        job.required_skills.map((item, index) => (
-                            <li className="row" key={index}>
-                                <div className="col-md-3">
-                                    <ShowEnumFromString labelPrefix="JobEquipmentType" str={item.type} enumArray={JobEquipmentType} />
-                                </div>
-                                <div className="col-md-3">
-                                    {`${item.years} ${t('YEARS')} `}
-                                </div>
-                            </li>
-                        ))
-                    }
-                    <li className="row">
-                        <div className="col-md-3">
-                            {t('OTHER')}
-                        </div>
-                        <div className="col-md-6">
-                            {job.required_skills_other}
-                        </div>
-                    </li>
-                </ul>
+                <div className="shadow-sm p-3 mb-5 bg-white rounded">
+                    <h3>{t('job_description')}</h3>
+                    <p>{job.description_short}</p>
+                    <p>{job.description}</p>
+                </div>
+                <div className="shadow-sm p-3 mb-5 bg-white rounded mt-3">
+                    <h3 className="mt-5">{t('BENEFITS')}</h3>
+                    <p>
+                        <ShowEnumFromString labelPrefix="JobBenefits" str={job.benefits} enumArray={JobBenefits} />
+                        , {job.benefits_other}
+                    </p>
+                </div>
+                <div className="shadow-sm p-3 mb-5 bg-white rounded mt-3">
+                    <h3 className="mt-5">{t('REQUIRED_SKILLS')}</h3>
+                    <ul>
+                        {job.required_skills &&
+                            job.required_skills.map((item, index) => (
+                                <li className="row" key={index}>
+                                    <div className="col-md-3">
+                                        <ShowEnumFromString labelPrefix="JobEquipmentType" str={item.type} enumArray={JobEquipmentType} />
+                                    </div>
+                                    <div className="col-md-3">
+                                        {`${item.years} ${t('YEARS')} `}
+                                    </div>
+                                </li>
+                            ))
+                        }
+                        <li className="row">
+                            <div className="col-md-3">
+                                {t('OTHER')}
+                            </div>
+                            <div className="col-md-6">
+                                {job.required_skills_other}
+                            </div>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </>
     )
