4b77c95208a64d4b91fe7e7df24ce44e8f6b5d11
Khuram K <khurramk@codeupscale.com>
2022-08-22 23:11:02 +0500

    DRIV-841 | preferred location fixes

--- pages/dashboard/driver/settings/applicant.tsx
+++ pages/dashboard/driver/settings/applicant.tsx
@@ -37,6 +37,7 @@ import { useEffect } from "react";
 import { toast, ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css'
 import { useEffectAsync } from "../../../../utils/react";
+import { JobGeography } from "../../../../enums/jobs/job-geography.enum";
 
 export default function Applicant() {
     const { t } = useTranslation();
@@ -167,6 +168,16 @@ export default function Applicant() {
                                         formik={form}
                                     />
                                 </Row>
+                                <BaseSelect
+                                    className="mt-1"
+                                    label="PREFERRED_LOCATION"
+                                    placeholder="PREFERRED_LOCATION"
+                                    name="preferred_location"
+                                    required
+                                    formik={form}
+                                    labelPrefix="JobGeography"
+                                    enumType={JobGeography}
+                                />
                             </Col>
                             <Col md="4" className="px-2">
                                 <BaseInput
@@ -210,7 +221,7 @@ export default function Applicant() {
                                     placeholder="years_cdl_experience"
                                     formik={form}
                                 />
-                                    <BaseCheckList
+                                <BaseCheckList
                                     className="col-12 p-1 "
                                     label="License_Restrictions"
                                     name="license_restrictions"
