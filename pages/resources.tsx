c93d14036fb6af265b2c8c6387a1daa5d6dcf899
irfan <irfan@codeupscale.com>
2022-09-06 13:24:42 +0500

    DRIV-875 PHASE 2 | Main Page | Need Driver Resources Page

--- pages/resources.tsx
+++ pages/resources.tsx
@@ -3,12 +3,23 @@ import { PublicLayout } from "../components/layouts/PublicLayout";
 import { Accordion, Container } from 'react-bootstrap';
 import Breadcrumb from "../components/breadcrumbs/Breadcrumb";
 import FreeResources from '../components/free-resources';
+import { useTranslation } from "../hooks/useTranslation";
+
 
 
 export default function FreeResource() {
+    const { t } = useTranslation();
     return (
         <>
-            <Container>
+            <div className="top-links-sec">
+                <div className="container">
+                    <div className="top-links-inner d-flex align-items-center justify-content-between">
+                        <h2>{t("RESOURCES")}</h2>
+                        < Breadcrumb />
+                    </div>
+                </div>
+            </div>
+            <Container fluid className='my-5'>
                 <FreeResources />
             </Container>
         </>
@@ -16,7 +27,7 @@ export default function FreeResource() {
 }
 FreeResource.getLayout = function getLayout(page) {
     return (
-        <PublicLayout title="FAQ">
+        <PublicLayout title="Resources">
             {page}
         </PublicLayout>
     )
