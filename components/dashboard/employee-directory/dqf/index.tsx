93b5a18d95e7166b69457830d8c9699560e9efa6
talhabhuttacodeup <talha.bhutta@codeupscale.com>
2022-09-27 17:46:29 +0500

    DRIV-905 | UI changes

--- components/dashboard/employee-directory/dqf/index.tsx
+++ components/dashboard/employee-directory/dqf/index.tsx
@@ -7,7 +7,7 @@ import JobApi from "../../../../pages/api/job";
 import { useEffectAsync } from "../../../../utils/react";
 import { useTranslation } from "../../../../hooks/useTranslation";
 import { ViewApplicantDetailProps } from "../../../../types/applicant/view-application-detail-props.type";
-import { useFormik } from "formik";
+import { Formik, useFormik } from "formik";
 import { ApplicantEntity } from "../../../../models/applicant/applicant.entity";
 import { DocumentEntity } from "../../../../models/documents/document.entity";
 import ApplicantApi from "../../../../pages/api/applicant";
@@ -38,6 +38,9 @@ const DqfTab = ({ applicant }: DqfTabProps) => {
         initialValues: new DocumentEntity(),
         validationSchema: DocumentEntity.yupSchema(),
         onSubmit: async (values, { resetForm }) => {
+            debugger
+            console.log("clickeeeeeeeeeeeeddddddddd")
+
             // try {
             // const applicantDocumentUpload = await applicantApi.documents.create(applicant.id, values)
 
@@ -48,7 +51,6 @@ const DqfTab = ({ applicant }: DqfTabProps) => {
             //     globalAjaxExceptionHandler(e, { formik: form, toast: toast, t: t });
             //     // if (e.response?.data?.message == "ApplicantJobService.APPLICANT_ALREADY_APPLIED") setViewForm(false)
             // }
-            console.log("clickeeeeeeeeeeeeddddddddd", values)
         }
     });
 
@@ -64,6 +66,7 @@ const DqfTab = ({ applicant }: DqfTabProps) => {
         // const upload = await applicantApi.documents.create(applicant.id, dto) 
         console.log("document Indexxxxx", docIndex, "applicant Id", applicant.id, "dto")
     }
+
     useEffectAsync(async () => {
         const v = await applicantApi.getById(applicant.id)
         applicant.documents = v.documents
@@ -91,20 +94,21 @@ const DqfTab = ({ applicant }: DqfTabProps) => {
                                 <tbody>
                                     {
                                         Object.values(ApplicantDocumentType).map((value: ApplicantDocumentType, i) => {
+                                            // debugger
                                             const document: any = applicant?.documents?.filter(v => (v.type === value))
-                                            console.log("document", document[0])
+                                            // console.log("document", document[0])
 
                                             return <tr key={i} className="testing_tr">
                                                 <td>
                                                     {t(`ApplicantDocumentType.${value}`)}
                                                 </td>
                                                 <td>
-                                                    {document[0]?.last_updated_at ? <ShowFormattedDate date={document[0]?.last_updated_at} /> : null}
+                                                    {document[0]?.last_updated_at ? <ShowFormattedDate date={document[0]?.last_updated_at} /> : <span className="text-danger font-italic">Not Available</span>}
                                                 </td>
                                                 <td>
                                                     {
                                                         (!form.values.type || form.values.type !== value) &&
-                                                        <Button className="mr-2"
+                                                        <Button className="mr-2 w-100"
                                                             onClick={() => { handleUpdateDocument(value, document[0]?.id) }}
                                                         >
                                                             {t(document[0] ? `EDIT` : `CREATE`)}
@@ -112,26 +116,28 @@ const DqfTab = ({ applicant }: DqfTabProps) => {
                                                     }
                                                     {
                                                         (form.values.type && form.values.type === value) && <>
-                                                            <Form onSubmit={form.handleSubmit}>
+                                                            <Form onSubmit={form.handleSubmit} >
                                                                 <FileInput
-                                                                    // className="col-12 my-3"
-                                                                    label={`pdf`}
                                                                     name={`file`}
                                                                     accept="application/pdf"
                                                                     documentType={"PDF"}
                                                                     formik={form}
                                                                 />
-                                                                <Button
-                                                                    className="mr-2"
-                                                                    type="submit"
-                                                                >
-                                                                    {t(`SAVE`)}
-                                                                </Button>
-                                                            </Form>
-                                                            <Button className="mr-2" onClick={() => { form.resetForm() }}                                                            >
-                                                                {t(`CANCEL`)}
-                                                            </Button>
+                                                                <div className="mt-2 d-flex w-100 ">
+                                                                    <Button
+                                                                        className="mr-2 w-50 bg-success"
+                                                                        type="submit"
+                                                                    // formik={form}
+                                                                    // onClick={() => {form.handleSubmit}}
+                                                                    >
+                                                                        {t(`SAVE`)}
+                                                                    </Button>
 
+                                                                    <Button type="button" className="mr-2 w-50 bg-danger" onClick={() => { form.resetForm() }}                                                            >
+                                                                        {t(`CANCEL`)}
+                                                                    </Button>
+                                                                </div>
+                                                            </Form>
                                                         </>
                                                     }
 
@@ -205,10 +211,11 @@ const DqfTab = ({ applicant }: DqfTabProps) => {
                                 </Table>
                             }
                         </ViewCard>
-                        <Button className="button_upload_document w-100 bg-success p-2" onClick={() => form.handleSubmit()}>{t("submit")}</Button>
+                        Button className="button_upload_document w-100 bg-success p-2" onClick={() => form.handleSubmit()}>{t("submit")}</Button>
 
                     </Col>
                 </Row> */}
+
             </div>
         </>
     );
