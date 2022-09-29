cddc074113967129af0f6259b4c5f4963ca4bcd8
talhabhuttacodeup <talha.bhutta@codeupscale.com>
2022-09-29 17:33:49 +0500

    DRIV-489 | tansition DAC in process

--- components/dashboard/employee-directory/dac/index.tsx
+++ components/dashboard/employee-directory/dac/index.tsx
@@ -9,9 +9,8 @@ import { ApplicantDacEntity } from "../../../../models/applicant/applicant-dac.e
 import ApplicantApi from "../../../../pages/api/applicant";
 import { globalAjaxExceptionHandler } from "../../../../utils/ajax";
 import { toast } from "react-toastify";
-import { useState } from "react";
-import { Pen } from "react-bootstrap-icons";
 import BaseCheck from "../../../forms/BaseCheck";
+import Collapse from 'react-bootstrap/Collapse';
 
 export interface DacTabProps extends ViewApplicantDetailProps { }
 
@@ -19,9 +18,6 @@ const DAC = ({ applicant }: DacTabProps) => {
 
     const { t } = useTranslation();
     const applicantApi = new ApplicantApi();
-    const [disableInput, setDisableInput] = useState<boolean>(true)
-
-    console.log("form valuesss applicant", applicant)
 
     const form = useFormik({
         initialValues: new ApplicantDacEntity(),
@@ -73,41 +69,43 @@ const DAC = ({ applicant }: DacTabProps) => {
                                             <td>
                                                 <div className="w-100">
                                                     {
-                                                        (!form.values.type || form.values.type !== value) &&
-                                                        <div className="d-flex">
+                                                        (!form.values.type || form.values.type !== value) ? (
+                                                            <div className="d-flex justify-content-between">
                                                             <BaseCheck
                                                                 readOnly
                                                                 disabled
-                                                                className="col-md-12"
                                                                 checked={dac?.type && dac.type == value && dac.value}
                                                             />
                                                             <Button
-                                                                className="mr-2 w-100"
+                                                                className="ml-2 w-50"
                                                                 onClick={() => { handleChangeClick(value, dac) }}
+                                                               
                                                             >
                                                                 {t('CHANGE')}
                                                             </Button>
                                                         </div>
+                                                        ) : (
+                                                            (form.values?.type === value) &&
+                                                            <Form onSubmit={form.handleSubmit} >
+                                                                <BaseCheck
+                                                                    name={`value`}
+                                                                    formik={form}
+                                                                />
+                                                                <Collapse in={form.values?.type === value} dimension="height">
+                                                                    <div className="d-flex justify-content-end w-100 " >
+                                                                        <Button className=" theme-primary-btn" type="submit">
+                                                                            {t(`SAVE`)}
+                                                                        </Button>
+                                                                        <Button type="button" className="ml-2 bg-danger" onClick={() => { form.resetForm() }}                                                            >
+                                                                            {t(`CANCEL`)}
+                                                                        </Button>
+                                                                    </div>
+                                                                </Collapse>
+                                                            </Form>
+                                                        )
+                                                      
                                                     }
 
-                                                    {
-                                                        (form.values?.type === value) &&
-                                                        <Form onSubmit={form.handleSubmit} >
-                                                            <BaseCheck
-                                                                className="col-md-12"
-                                                                name={`value`}
-                                                                formik={form}
-                                                            />
-                                                            <div className="mt-2 d-flex w-100 ">
-                                                                <Button className="mr-2 w-50 theme-primary-btn" type="submit">
-                                                                    {t(`SAVE`)}
-                                                                </Button>
-                                                                <Button type="button" className="mr-2 w-50 bg-danger" onClick={() => { form.resetForm() }}                                                            >
-                                                                    {t(`CANCEL`)}
-                                                                </Button>
-                                                            </div>
-                                                        </Form>
-                                                    }
                                                 </div>
                                             </td>
 
