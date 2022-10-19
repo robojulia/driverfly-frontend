a0be97a6db03cf34fbe0ef5fc19829c38eadf580
Khuram K <khurramk@codeupscale.com>
2022-10-19 18:10:40 +0500

    DRIV-978 | Create Job | unable to add experience fixes

--- models/job/job-skill.entity.ts
+++ models/job/job-skill.entity.ts
@@ -2,15 +2,29 @@ import { JobEquipmentType } from '../../enums/jobs/job-equipment-type.enum';
 import * as yup from "yup";
 
 export class JobSkillEntity {
-  type?: JobEquipmentType;
-  years?: number;
-  months?: number;
+    type?: JobEquipmentType;
+    years?: number;
+    months?: number;
 
-  static yupSchema() {
-    return yup.object({
-        type: yup.string().required().nullable(),
-        years: yup.number().min(1).required().nullable(),
-        months: yup.number().min(1).max(11).nullable(),
-    });
-  }
+    static yupSchema() {
+        return yup.object()
+            .shape({
+                type: yup.string().required().nullable(),
+                years: yup.number()
+                    .when("months", {
+                        is: v => v > 0,
+                        then: yup.number().optional().min(0).nullable(),
+                        otherwise: yup.number().required().min(1).nullable(),
+                    })
+                    .nullable(),
+                months: yup.number()
+                    .when("years", {
+                        is: v => v > 0,
+                        then: yup.number().optional().min(0).nullable(),
+                        otherwise: yup.number().required().min(1).nullable(),
+                    })
+                    .max(11)
+                    .nullable(),
+            }, [['months', 'years']]);
+    }
 }
