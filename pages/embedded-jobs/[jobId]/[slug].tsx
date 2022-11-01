3baaca6c9f941bb045b58836c018655d369b7304
irfan <irfan@codeupscale.com>
2022-11-01 12:23:35 +0500

    DRIV-556 Created layout for embedded pages, components creation and integrate components for jon-listing and job detail (Embedded)pages

---
+++ pages/embedded-jobs/[jobId]/[slug].tsx
@@ -0,0 +1,53 @@
+import { EmbeddedLayout } from "../../../components/layouts/embedded/embedded-layout";
+import RelatedJobs from '../../../components/related-jobs/related-jobs'
+import { useTranslation } from "../../../hooks/use-translation"
+import JobApi from "../../api/job"
+import StructuredData from "../../../components/seo/structured-data"
+import ViewJobDetail from "../../../components/jobs/view-job-detail";
+import { JobDetailProps } from "../../../types/job/job-detail-props.type";
+
+export default function Detail({ job, relatedJobs }: JobDetailProps) {
+
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <StructuredData type="JobPosting" data={StructuredData.JobPosting(job, t)} />
+      <ViewJobDetail
+        job={job}
+        relatedJobs={< RelatedJobs jobs={relatedJobs} jobLink="jobs" hideCompanyName={false} jobLinkSlugable={true} />}
+        canApply={true}
+        canSave={true}
+        hideVehicles={false}
+        hideCompanyName={true}
+      />
+    </>
+  )
+}
+export async function getServerSideProps(context) {
+  try {
+    const jobId = context.params?.jobId;
+    if (!!!jobId)
+      return { notFound: true }
+
+    const job = await new JobApi().getById(jobId)
+    if (!!!job)
+      return { notFound: true }
+
+    const { items } = await new JobApi().search({ exclude: { jobId: jobId }, companyId: job.company?.id, take: 3 });
+    return {
+      props: { job: job, relatedJobs: items }
+    }
+  } catch (error) {
+    console.error("Exception is here:", error);
+    return { props: { job: {}, relatedJobs: [] } }
+  }
+}
+
+Detail.getLayout = function getLayout(page) {
+  return (
+    <EmbeddedLayout>
+      {page}
+    </EmbeddedLayout>
+  )
+}
---
+++ pages/embedded/index.tsx
@@ -0,0 +1,135 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import { ChangeEvent, useState } from "react"
+import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css'
+import { EmbeddedLayout } from '../../components/layouts/embedded/embedded-layout'
+import JobApi from "../api/job"
+import { JobEntity } from '../../models/job/job.entity'
+import { filtersInitialsValues, pagingMetaInitialValues, PagingMetaProps } from '../../utils/job-filter'
+import { useEffectAsync } from '../../utils/react'
+import { GetServerSidePropsContext } from 'next'
+import { toast } from "react-toastify";
+import { useTranslation } from '../../hooks/use-translation'
+import PageLayout from '../../components/layouts/page/page-layout'
+import jobsContext from "../../context/job-context"
+import { JobSearchLocation, SearchJobsDto } from "../../models/job/search-jobs-dto";
+import ResultCount from "../../components/find-jobs/result-count"
+import Filters from "../../components/dashboard/driver/find-job/filters";
+import OtrJobsList from "../../components/find-jobs/otr-job-list";
+import { JobGeography } from "../../enums/jobs/job-geography.enum";
+import {Row, Col } from 'react-bootstrap';
+import JobsList from '../../components/embedded-jobs-listing/jobs-list'
+
+
+export default function Embedded(props) {
+
+    const jobApi = new JobApi()
+    const { t } = useTranslation();
+
+    const [jobs, setJobs] = useState<JobEntity[]>([])
+
+    const [pagingMeta, setPagingMeta] = useState<PagingMetaProps>(pagingMetaInitialValues)
+    const resetPagingMeta = (): void => setPagingMeta(pagingMetaInitialValues)
+
+    const [searchQuery, setSearchQuery] = useState<string>()
+    const resetSearchQuery = (): void => setSearchQuery('')
+
+    const [filters, setFilters] = useState<SearchJobsDto>(filtersInitialsValues)
+    const resetFilters = (): void => setFilters(filtersInitialsValues)
+
+    const [location, setLocation] = useState<JobSearchLocation>(null)
+    const resetLocation = (): void => setLocation(null)
+
+    const [range, setRange] = useState<string>(`${filters.location?.range || 50}`)
+    const resetRange = (): void => setRange(null)
+
+    const handleReset = (): void => {
+        resetSearchQuery()
+        resetPagingMeta()
+        resetFilters()
+        resetLocation()
+        resetRange()
+    }
+
+    const setFiltersByKeyValue = (key: string, value: any): void => {
+        setFilters({
+            ...filters,
+            page: 1,
+            [key]: value
+        })
+    }
+
+    const handleChange = ({ target: { name, value } }: ChangeEvent<HTMLInputElement>): void => setFiltersByKeyValue(name, value)
+
+    const fetchJobs = async (): Promise<void> => {
+        try {
+            await jobApi.search({ ...filters as any })
+                .then(({ items, meta }) => {
+                    setJobs(items)
+                    setPagingMeta(meta)
+                })
+        } catch (e) {
+            toast.error(t('FIND_JOB_ERROR_GENERAL'))
+        }
+    }
+
+    useEffectAsync(fetchJobs, [filters]);
+    return (
+        <PageLayout>
+            <jobsContext.Provider value={{
+                state: {
+                    jobs,
+                    pagingMeta,
+                    filters,
+                    location,
+                    range,
+                    searchQuery,
+                },
+                method: {
+                    handleChange,
+                    handlePaging: setPagingMeta,
+                    setFilters,
+                    setLocation,
+                    setRange,
+                    setFiltersByKeyValue,
+                    applyFilters: fetchJobs,
+                    setSearchQuery,
+                    handleReset
+                },
+            }}>
+                <div className="job-list_sec container mt-5">
+                    <Filters />
+                    <Row className='mt-5'>
+                        <Col className='col-12 my-lg-0 my-4 p-0'>
+                            <ResultCount />
+                            < JobsList />
+                        </Col>
+                    </Row>
+                    {
+                        (!!filters.location && !!filters.areas_covered && filters.areas_covered == JobGeography.OTR) &&
+                        <Row className='mt-5'>
+                            <Col className='col-12 my-lg-0 my-4'>
+                                < OtrJobsList />
+                            </Col>
+                        </Row>
+                    }
+                </div>
+            </jobsContext.Provider>
+        </PageLayout>
+    )
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+    return {
+        props: {
+            params: context.query
+        }
+    }
+}
+
+Embedded.getLayout = function getLayout(page) {
+    return (
+        <EmbeddedLayout>
+            {page}
+        </EmbeddedLayout>
+    )
+}
