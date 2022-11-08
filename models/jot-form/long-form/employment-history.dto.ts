a4af0178a7deace55c42a01dcba896ed170f2c8b
Khuram K <khurramk@codeupscale.com>
2022-11-08 18:01:50 +0500

    DRIV-986 | EmploymentHistory schema defined

--- models/jot-form/long-form/employment-history.dto.ts
+++ models/jot-form/long-form/employment-history.dto.ts
@@ -1,60 +1,14 @@
 import moment from "moment";
 import * as yup from "yup";
+import { ApplicantExtras } from "../../../enums/applicants/applicant-extras.enum";
+import { ApplicantExtrasEntity } from "../../applicant/applicant-extras.entity";
 
 export class EmploymentHistoryDto {
-  employed_type: string;
-  current_company_manager_name: string;
-  current_company_phone_number: number;
-  current_company_email: string;
-  current_company_street_address_line_1: string;
-  current_company_street_address_line_2: string;
-  current_company_zipcode: number;
-  static yupSchema() {
-    return yup.object({
-      current_company_manager_name: yup
-        .string()
-        .when("employed_type", {
-          is: (v) => !!v,
-          then: yup.string().required().nullable(),
-          otherwise: yup.string().optional().nullable(),
-        })
-        .nullable(),
+    CURRENT_EMPLOYER: ApplicantExtrasEntity;
 
-      current_company_email: yup
-        .string()
-        .when("employed_type", {
-          is: (v) => !!v,
-          then: yup.string().required().nullable(),
-          otherwise: yup.string().optional().nullable(),
-        })
-        .nullable(),
-
-      current_company_street_address_line_1: yup
-        .string()
-        .when("employed_type", {
-          is: (v) => !!v,
-          then: yup.string().required().nullable(),
-          otherwise: yup.string().optional().nullable(),
-        })
-        .nullable(),
-
-      current_company_street_address_line_2: yup
-        .string()
-        .when("employed_type", {
-          is: (v) => !!v,
-          then: yup.string().required().nullable(),
-          otherwise: yup.string().optional().nullable(),
-        })
-        .nullable(),
-
-      current_company_zipcode: yup
-        .string()
-        .when("employed_type", {
-          is: (v) => !!v,
-          then: yup.string().required().nullable(),
-          otherwise: yup.string().optional().nullable(),
-        })
-        .nullable(),
-    });
-  }
+    static yupSchema() {
+        return yup.object({
+            CURRENT_EMPLOYER: ApplicantExtrasEntity.yupSchema()
+        });
+    }
 }
--- models/jot-form/long-form/emplyment-history/index.dto.ts
+++ models/jot-form/long-form/emplyment-history/index.dto.ts
@@ -2,119 +2,121 @@ import * as yup from "yup";
 import "../../../../utils/yup";
 
 export class EmploymentHistoryExtraDto {
-  employed_type: boolean | boolean = false;
-  current_company_manager_name: string;
-  start_date: string | Date;
-  authorize: boolean | boolean = false;
-  current_company_phone_number: number;
-  current_company_email: string;
-  current_company_street_address_line_1: string;
-  current_company_street_address_line_2: string;
-  current_company_zipcode: number;
-  city: string;
-  state: string;
-  fmcsr: string;
-  fcr: string;
+    is_current_employed: boolean;
+    current_company_manager_name: string;
+    start_date: string | Date;
+    authorize: boolean | boolean = false;
+    current_company_phone_number: number;
+    current_company_email: string;
+    current_company_street_address_line_1: string;
+    current_company_street_address_line_2: string;
+    current_company_zipcode: number;
+    city: string;
+    state: string;
+    fmcsr: string;
+    fcr: string;
 
-  static yupSchema() {
-    return yup.object({
-      current_company_manager_name: yup
-        .string()
-        .when("employed_type", {
-          is: (v) => !!v,
-          then: yup.string().required().nullable(),
-          otherwise: yup.string().optional().nullable(),
-        })
-        .nullable(),
+    static yupSchema() {
+        return yup.object({
+            is_current_employed: yup.boolean().optional().nullable(),
 
-      city: yup
-        .string()
-        .when("employed_type", {
-          is: (v) => !!v,
-          then: yup.string().required().nullable(),
-          otherwise: yup.string().optional().nullable(),
-        })
-        .nullable(),
+            current_company_manager_name: yup
+                .string()
+                .when("is_current_employed", {
+                    is: (v) => !!v,
+                    then: yup.string().required().nullable(),
+                    otherwise: yup.string().optional().nullable(),
+                })
+                .nullable(),
 
-      fmcsr: yup
-        .string()
-        .when("employed_type", {
-          is: (v) => !!v,
-          then: yup.string().required().nullable(),
-          otherwise: yup.string().optional().nullable(),
-        })
-        .nullable(),
+            city: yup
+                .string()
+                .when("is_current_employed", {
+                    is: (v) => !!v,
+                    then: yup.string().required().nullable(),
+                    otherwise: yup.string().optional().nullable(),
+                })
+                .nullable(),
 
-      fcr: yup
-        .string()
-        .when("employed_type", {
-          is: (v) => !!v,
-          then: yup.string().required().nullable(),
-          otherwise: yup.string().optional().nullable(),
-        })
-        .nullable(),
+            fmcsr: yup
+                .string()
+                .when("is_current_employed", {
+                    is: (v) => !!v,
+                    then: yup.string().required().nullable(),
+                    otherwise: yup.string().optional().nullable(),
+                })
+                .nullable(),
 
-      state: yup
-        .string()
-        .when("employed_type", {
-          is: (v) => !!v,
-          then: yup.string().required().nullable(),
-          otherwise: yup.string().optional().nullable(),
-        })
-        .nullable(),
+            fcr: yup
+                .string()
+                .when("is_current_employed", {
+                    is: (v) => !!v,
+                    then: yup.string().required().nullable(),
+                    otherwise: yup.string().optional().nullable(),
+                })
+                .nullable(),
 
-      current_company_email: yup
-        .string()
-        .when("employed_type", {
-          is: (v) => !!v,
-          then: yup.string().required().nullable(),
-          otherwise: yup.string().optional().nullable(),
-        })
-        .nullable(),
-      start_date: yup
-        .string()
-        .when("employed_type", {
-          is: (v) => !!v,
-          then: yup.string().required().nullable(),
-          otherwise: yup.string().optional().nullable(),
-        })
-        .nullable(),
+            state: yup
+                .string()
+                .when("is_current_employed", {
+                    is: (v) => !!v,
+                    then: yup.string().required().nullable(),
+                    otherwise: yup.string().optional().nullable(),
+                })
+                .nullable(),
 
-      authorize: yup
-        .string()
-        .when("employed_type", {
-          is: (v) => !!v,
-          then: yup.boolean().default(false).required().nullable(),
-          otherwise: yup.boolean().default(false).optional().nullable(),
-        })
-        .nullable(),
+            current_company_email: yup
+                .string()
+                .when("is_current_employed", {
+                    is: (v) => !!v,
+                    then: yup.string().required().nullable(),
+                    otherwise: yup.string().optional().nullable(),
+                })
+                .nullable(),
+            start_date: yup
+                .string()
+                .when("is_current_employed", {
+                    is: (v) => !!v,
+                    then: yup.string().required().nullable(),
+                    otherwise: yup.string().optional().nullable(),
+                })
+                .nullable(),
 
-      current_company_street_address_line_1: yup
-        .string()
-        .when("employed_type", {
-          is: (v) => !!v,
-          then: yup.string().required().nullable(),
-          otherwise: yup.string().optional().nullable(),
-        })
-        .nullable(),
+            authorize: yup
+                .string()
+                .when("is_current_employed", {
+                    is: (v) => !!v,
+                    then: yup.boolean().default(false).required().nullable(),
+                    otherwise: yup.boolean().default(false).optional().nullable(),
+                })
+                .nullable(),
 
-      current_company_street_address_line_2: yup
-        .string()
-        .when("employed_type", {
-          is: (v) => !!v,
-          then: yup.string().required().nullable(),
-          otherwise: yup.string().optional().nullable(),
-        })
-        .nullable(),
+            current_company_street_address_line_1: yup
+                .string()
+                .when("is_current_employed", {
+                    is: (v) => !!v,
+                    then: yup.string().required().nullable(),
+                    otherwise: yup.string().optional().nullable(),
+                })
+                .nullable(),
 
-      current_company_zipcode: yup
-        .string()
-        .when("employed_type", {
-          is: (v) => !!v,
-          then: yup.string().required().nullable(),
-          otherwise: yup.string().optional().nullable(),
-        })
-        .nullable(),
-    });
-  }
+            current_company_street_address_line_2: yup
+                .string()
+                .when("is_current_employed", {
+                    is: (v) => !!v,
+                    then: yup.string().required().nullable(),
+                    otherwise: yup.string().optional().nullable(),
+                })
+                .nullable(),
+
+            current_company_zipcode: yup
+                .string()
+                .when("is_current_employed", {
+                    is: (v) => !!v,
+                    then: yup.string().required().nullable(),
+                    otherwise: yup.string().optional().nullable(),
+                })
+                .nullable(),
+        });
+    }
 }
