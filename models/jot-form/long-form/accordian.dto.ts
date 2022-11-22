85d6aaa6c7766d1a2937aa33b1b0376bd1fc3662
Talha Bhutta <junaid@192.168.1.11>
2022-11-22 13:56:18 +0500

    DRIV-1012 | integration ACCORDIAN page bug fixes | error resolved | PR merged

--- models/jot-form/long-form/accordian.dto.ts
+++ models/jot-form/long-form/accordian.dto.ts
@@ -1,14 +1,20 @@
 import * as yup from "yup";
-import { ApplicantExtras } from "../../../enums/applicants/applicant-extras.enum";
 import { ApplicantExtrasEntity } from "../../applicant/applicant-extras.entity";
-import { ApplicantAccordion } from "../../../enums/applicants/applicant-accordion.enum";
 
 export class AccordianDto {
-    SIGNATURE: ApplicantExtrasEntity;
+  SIGNATURE: ApplicantExtrasEntity;
+  EMPLOYEE_SS_OR_ID: ApplicantExtrasEntity;
+  DISCLOSURE_AND_AUTHORIZATION_DATE: ApplicantExtrasEntity;
+  IMPORTANT_DISCLOSURE_BACKGROUND_DATE: ApplicantExtrasEntity;
+  GENERAL_CONSENT: ApplicantExtrasEntity;
 
-    static yupSchema(){
-        return yup.object({
-            SIGNATURE: ApplicantExtrasEntity.yupSchema()
-        });
-    }
+  static yupSchema() {
+    return yup.object({
+      SIGNATURE: ApplicantExtrasEntity.yupSchema(),
+      EMPLOYEE_SS_OR_ID: ApplicantExtrasEntity.yupSchema(),
+      DISCLOSURE_AND_AUTHORIZATION_DATE: ApplicantExtrasEntity.yupSchema(),
+      IMPORTANT_DISCLOSURE_BACKGROUND_DATE: ApplicantExtrasEntity.yupSchema(),
+      GENERAL_CONSENT: ApplicantExtrasEntity.yupSchema(),
+    });
+  }
 }
