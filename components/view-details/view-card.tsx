49dbd96a791ac953cfaa7bd74560a0dead3c6b8d
talhabhuttacodeup <talha.bhutta@codeupscale.com>
2022-12-09 15:12:57 +0500

    DRIV-1033 | style fixes

--- components/view-details/view-card.tsx
+++ components/view-details/view-card.tsx
@@ -2,35 +2,33 @@ import { useTranslation } from "../../hooks/use-translation";
 import { Card } from "react-bootstrap";
 
 export interface ViewCardProps {
-    title?: string;
-    titleAs?: React.ElementType;
-    variant?: "primary" | "secondary",
-    readonly children?: any;
-    actions?: JSX.Element | JSX.Element[];
+  title?: string;
+  titleAs?: React.ElementType;
+  variant?: "primary" | "secondary";
+  readonly children?: any;
+  actions?: JSX.Element | JSX.Element[];
 }
 
 export default function ViewCard(props: ViewCardProps) {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    let { title, actions, children, variant, titleAs: TitleAsCmp } = props;
+  let { title, actions, children, variant, titleAs: TitleAsCmp } = props;
 
-    function renderTitleCmp() {
-        if (TitleAsCmp) return <TitleAsCmp>{t(title)}</TitleAsCmp>
+  function renderTitleCmp() {
+    if (TitleAsCmp) return <TitleAsCmp>{t(title)}</TitleAsCmp>;
 
-        return <h5>{t(title)}</h5>
-    }
+    return <h5>{t(title)}</h5>;
+  }
 
-    return (
-        <Card className={`card-${variant || "primary"} `}
-        >
-            {(title || actions) && <Card.Header>
-                {title && <div style={{float: "left"}}>
-                    {renderTitleCmp()}
-                </div>}
-                {actions && <div style={{float: "right"}}>{actions}</div>}
-            </Card.Header>}
-            {children && <Card.Body className="my_card_body">{children}</Card.Body>}
-        </Card>
-    );
-
+  return (
+    <Card className={`card-${variant || "primary"} `}>
+      {(title || actions) && (
+        <Card.Header>
+          {title && <div style={{ float: "left" }}>{renderTitleCmp()}</div>}
+          {actions && <div style={{ float: "right" }}>{actions}</div>}
+        </Card.Header>
+      )}
+      {children && <Card.Body className="my_card_body">{children}</Card.Body>}
+    </Card>
+  );
 }
