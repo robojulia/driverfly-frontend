ae942d489fcfcb2540262e96d54624c810588a28
Khuram K <khurramk@codeupscale.com>
2022-12-09 17:48:35 +0500

    DRIV-1023 | MinimumYearsExperience filter completed

--- components/embedded-filters/embedded-filters.tsx
+++ components/embedded-filters/embedded-filters.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import { useContext, useState } from 'react'
 import AreasCovered from '../filters/areas-covered'
 import EmploymentType from '../filters/employment-type'
 import MvrRequirement from '../filters/mvr-requirements'
--- components/filters/minimum-years-experience.tsx
+++ components/filters/minimum-years-experience.tsx
@@ -1,76 +1,77 @@
-import { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { InputGroup } from "react-bootstrap";
 import { useTranslation } from "../../hooks/use-translation";
 import FindJobFilterAccordion from "../find-jobs-accordion/find-job-filter-accordion";
 import BaseInput from "../forms/base-input";
 
+export type MinimumExperienceType = {
+    months: number,
+    years: number
+}
 export default function MinimumYearsExperience(props) {
 
     const {
-        state: { filters, searchQuery },
-        method: { setFiltersByKeyValue, setSearchQuery },
-        labelClassName,
-        label,
+        state: { filters: { min_years_experience } },
+        method: { setFiltersByKeyValue },
     } = props
     const { t } = useTranslation()
 
-
-    const [minimumExperience, setMinimumExperience] = useState({
-        minimumMonthExperience: null,
-        minimumYearsExperience: null
+    const [minimumExperience, setMinimumExperience] = useState<MinimumExperienceType>({
+        months: min_years_experience ? Math.round((min_years_experience % 1) * 12) : null,
+        years: min_years_experience ? Math.floor(min_years_experience) : null
     })
+
+    const handleChange = ({ target: { name, value } }: React.ChangeEvent<HTMLInputElement>): void =>
+        setMinimumExperience({
+            ...minimumExperience,
+            [name]: value ? parseInt(value) : 0
+        })
 
 
     useEffect(() => {
         let minExperience: number = 0;
-        if (minimumExperience.minimumMonthExperience) {
-            minExperience = minimumExperience.minimumYearsExperience + minimumExperience.minimumMonthExperience / 12;
+        if (minimumExperience.months) {
+            minExperience = minimumExperience.years + (minimumExperience.months / 12);
         }
         else {
-            minExperience = minimumExperience.minimumMonthExperience;
+            minExperience = minimumExperience.years;
         }
 
         setFiltersByKeyValue('min_years_experience', minExperience)
 
     }, [minimumExperience])
 
-
-
     return (
-        <>
-            <FindJobFilterAccordion {...props} header={t("MIN_YEARS_EXPERIENCE")}>
-                <div className="custom-control custom-checkbox p-0">
-                    <div className="App">
-                        <InputGroup className="flex-nowrap rounded d-block">
-                            <BaseInput
-                                className="col-md-6 d-inline-block p-0 mb-2"
-                                placeholder="5"
-                                value={minimumExperience.minimumYearsExperience}
-                                name="min_experience_in_years"
-                                required
-                                min="0"
-                                type="int"
-                                onChange={e => setMinimumExperience({ ...minimumExperience, minimumYearsExperience: e.target.value })}
-                                append={(<InputGroup.Text>{t('YEARS_SHORT')}</InputGroup.Text>)}
-
-                            />
-                            <BaseInput
-                                className="col-md-6 d-inline-block p-0"
-                                placeholder="5"
-                                name="min_experience_in_months"
-                                required
-                                min="0"
-                                max="11"
-                                type="int"
-                                value={minimumExperience.minimumMonthExperience}
-                                append={(<InputGroup.Text>{t('MONTHS_SHORT')}</InputGroup.Text>)}
-                                onChange={(e) => setMinimumExperience({ ...minimumExperience, minimumMonthExperience: e.target.value })}
-                            />
-                        </InputGroup>
-                    </div>
+        <FindJobFilterAccordion {...props} header={t("MIN_YEARS_EXPERIENCE")}>
+            <div className="custom-control custom-checkbox p-0">
+                <div className="App">
+                    <InputGroup className="flex-nowrap rounded d-block">
+                        <BaseInput
+                            className="col-md-6 d-inline-block p-0 mb-2"
+                            placeholder="5"
+                            value={minimumExperience.years}
+                            name="years"
+                            required
+                            min="0"
+                            type="int"
+                            onChange={handleChange}
+                            append={(<InputGroup.Text>{t('YEARS_SHORT')}</InputGroup.Text>)}
+                        />
+                        <BaseInput
+                            className="col-md-6 d-inline-block p-0"
+                            placeholder="5"
+                            name="months"
+                            required
+                            min="0"
+                            max="11"
+                            type="int"
+                            value={minimumExperience.months}
+                            append={(<InputGroup.Text>{t('MONTHS_SHORT')}</InputGroup.Text>)}
+                            onChange={handleChange}
+                        />
+                    </InputGroup>
                 </div>
-            </FindJobFilterAccordion>
-
-        </>
+            </div>
+        </FindJobFilterAccordion>
     )
 }
