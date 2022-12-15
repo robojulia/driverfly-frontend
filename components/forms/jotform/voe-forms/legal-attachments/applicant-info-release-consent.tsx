7890cf44be10edd28f1163a73817566d29fd704e
irfan <irfan@codeupscale.com>
2022-12-15 19:07:21 +0500

    DRIV-1037 Combine Voe-attachment sec2 into sec2 and create new component and page for applicant-info-release-consent

---
+++ components/forms/jotform/voe-forms/legal-attachments/applicant-info-release-consent.tsx
@@ -0,0 +1,61 @@
+import { useTranslation } from "../../../../../hooks/use-translation";
+import { ApplicantExtras } from "../../../../../enums/applicants/applicant-extras.enum";
+import JotformContext, { JotFormContextType } from "../../../../../context/jotform-context";
+import { useContext } from "react";
+
+export default function ApplicantInfoReleaseConsent() {
+    const {
+        state: { applicantExtras, applicant }
+    }: JotFormContextType = useContext(JotformContext);
+    const { t } = useTranslation();
+    const date = applicant?.extras?.find(
+        (v) => v.type == ApplicantExtras.IMPORTANT_DISCLOSURE_BACKGROUND_DATE
+    );
+
+    return (
+        <form>
+            <div className="Row">
+                <div className="Col">
+                    <h1 style={{ fontWeight: 'bold', textAlign: "center" }}>
+                        {t("APPLICANT_INFORMATION_RELEASE_CONSENT_FORM")}
+                    </h1>
+                </div>
+            </div>
+            <div className="Row">
+                <p style={{ color: 'black', display: 'inline'}}>
+                    {t("I_HEREBT_AUTHORIZE_ANY_PERSONAL")}
+                </p>
+            </div>
+            <div className="Row" style={{ marginTop: '30px' }}>
+                <p style={{ color: 'black', fontWeight: 'bold' ,margin: '0px'}}>{t("TO_BE_READ_AND_SIGNED_BY_APPLICANT")}</p>
+                <p style={{ color: 'black', display: 'inline' }}>
+                    {t("IT_IS_AGREE_AND_UNDERSTOOD")}
+                </p>
+            </div>
+            <div className="Row" style={{ marginTop: '30px', marginBottom: '20px' }}>
+                <p style={{ color: 'black', display: 'inline' }}>
+                    {t(
+                        "{company_name}_I_HEREBY_AUTHORIZE_AND_REQUEST_FOLLOWING",
+                        { company_name: applicant?.company?.name },
+                        { translateProps: true }
+                    )}
+                </p>
+            </div>
+            <div className="Row" style={{ textAlign: 'left', marginBottom: '20px' }}>
+                <div className="Col">
+                    <p style={{ color: 'black', fontWeight: 'bold', display: 'inline' }}>{t("NAME:")}</p>
+                    <p style={{ color: 'black', display: 'inline' }}>{` ${applicant?.first_name} ${applicant?.last_name}`}</p>
+
+                </div>
+            </div>
+            <div className="Row" style={{ textAlign: 'left', marginBottom: '20px' }}>
+                <div className="Col">
+                    <p style={{ color: 'black', fontWeight: 'bold', display: 'inline' }}>{t("DATE:")}</p>
+                    <p style={{ color: 'black', display: 'inline' }}>{date?.value ? date?.value : ` ${t("NULL")}`}</p>
+
+                </div>
+            </div>
+
+        </form>
+    )
+}
