9457e21048ffeff8a68e91c538d90505c70b1007
irfan <irfan@codeupscale.com>
2023-01-03 19:36:33 +0500

    DRIV-1042 CDL class Schema fixes

--- models/contact/company-preferences.entity.ts
+++ models/contact/company-preferences.entity.ts
@@ -4,12 +4,12 @@ import { DriverLicenseType } from "../../enums/users/driver-license-type.enum";
 export class CompanyPreferencesEntity {
 
     jotform_url: string;
-    cdl_class?: DriverLicenseType;
+    cdl_class: DriverLicenseType;
 
     static yupSchema() {
         return yup.object({
             jotform_url: yup.string().trim().required().nullable(),
-            cdl_class: (yup.string() as any).enum(DriverLicenseType).nullable(),
+            cdl_class: yup.array((yup.string() as any).enum(DriverLicenseType)).nullable()
 
 
         });
