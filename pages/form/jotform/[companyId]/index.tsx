3363435c84a2d0211249dd86cf74e96dcf0e06a8
talhabhuttacodeup <talha.bhutta@codeupscale.com>
2023-01-13 13:58:39 +0500

    DRIV-986 | undefined Bug resolved

--- pages/form/jotform/[companyId]/index.tsx
+++ pages/form/jotform/[companyId]/index.tsx
@@ -24,7 +24,7 @@ export default function FullForm({ employer }: FullFormProps) {
 				? [...oldApx, { ...applicantExtrasEntity }]
 				: [{ ...applicantExtrasEntity }];
 		});
-	const [steps, setSteps] = useState<number>(26);
+	const [steps, setSteps] = useState<number>(0);
 	const stepNext = (): void => setSteps(steps + 1);
 	const stepBack = (): void => setSteps(steps - 1);
 
