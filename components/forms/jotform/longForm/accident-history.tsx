e039a56a2175340eed9fd8ac601d65f3503ef518
talhabhuttacodeup <talha.bhutta@codeupscale.com>
2023-01-30 18:30:24 +0500

    DRIV-1068 | issue fixed for conditional rendering

--- components/forms/jotform/longForm/accident-history.tsx
+++ components/forms/jotform/longForm/accident-history.tsx
@@ -157,35 +157,32 @@ export function AccidentHistory() {
 				</>
 			)}
 			{
-				console.log("form.values.ACCIDENT_DETAILS.value.", form.values.ACCIDENT_DETAILS?.value?.length)
-
-			}
-			{
-				console.log("form.values.accident_count", form.values.accident_count)
-			}
-			{
-				form.values.accident_count > 0 || form.values.accident_count < form.values.accident_count && (
+				form?.values?.accident_count > 0 && (
 					<>
-
-
-						<Row>
-							<div className="mt-4 float-left d-flex justify-left">
-								<Button
-									className="w-100 py-2"
-									size="sm"
-									onClick={() =>
-										form.setFieldValue("ACCIDENT_DETAILS.value", [
-											...(form.values?.ACCIDENT_DETAILS?.value || []),
-											new AccidentHistoryEntity(),
-										])
-									}
-								>
-									<PlusCircle /> {t("TITLE_ADD_ACCIDENT_DETAILS")}
-								</Button>
-							</div>
-						</Row>
+						{
+							Boolean(form?.values?.accident_count !== Object?.keys(form?.values?.ACCIDENT_DETAILS?.value || 0)?.length) && (
+								<>
+									<Row>
+										<div className="mt-4 float-left d-flex justify-left">
+											<Button
+												className="w-100 py-2"
+												size="sm"
+												onClick={() =>
+													form.setFieldValue("ACCIDENT_DETAILS.value", [
+														...(form.values?.ACCIDENT_DETAILS?.value || []),
+														new AccidentHistoryEntity(),
+													])
+												}
+											>
+												<PlusCircle /> {t("TITLE_ADD_ACCIDENT_DETAILS")}
+											</Button>
+										</div>
+									</Row>
+								</>
+							)}
 					</>
-				)}
+				)
+			}
 
 			<Row className="mt-4">
 				<Col>
