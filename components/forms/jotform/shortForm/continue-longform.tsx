57427629126159ce90c62fa0ccfa9b73b0c0a019
talhabhuttacodeup <talha.bhutta@codeupscale.com>
2023-01-31 16:19:26 +0500

    DRIV-1066 | message prompt of required cdl class after short form

--- components/forms/jotform/shortForm/continue-longform.tsx
+++ components/forms/jotform/shortForm/continue-longform.tsx
@@ -1,10 +1,14 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styles from "../../../../styles/digitalhiringapp.module.css";
 import { useTranslation } from "../../../../hooks/use-translation";
 import { useFormik } from "formik";
 import { Button, Col, Row } from "react-bootstrap";
 import JotformContext, { JotFormContextType } from "../../../../context/jotform-context";
 import { toast, ToastContainer } from "react-toastify";
+import CompanyApi from "../../../../pages/api/company";
+import { CompanyPreferenceCategory } from "../../../../enums/company/company-preference-category.enum";
+import { useEffectAsync } from "../../../../utils/react";
+import { CompanyPreferenceJotformLabel } from "../../../../enums/company/company-preferences-jotform-label.enum";
 
 
 export function ContinueLongForm() {
@@ -14,17 +18,30 @@ export function ContinueLongForm() {
 	}: JotFormContextType = useContext(JotformContext);
 
 	const { t } = useTranslation();
-
+	const [cdlCategory, setCdlCategory] = useState<[]>([])
 	const form = useFormik({
 		initialValues: {},
 		onSubmit: () => {
+			console.log(`applicant company ${applicant?.company?.name}`, applicant?.company?.id)
 			stepNext();
 		}
 	});
-	useEffect(() => {
-		toast.success(t("successfully_saved_information"));
+	useEffectAsync(async () => {
+		if (applicant?.company) {
+			const api = new CompanyApi();
+			const preferences = await api.preferences.list(applicant?.company?.id, {
+				category: CompanyPreferenceCategory.JOTFORM,
+			}).then((pref) => {
+				const category = pref?.find(v => v?.label === CompanyPreferenceJotformLabel.CDL_CLASS)
+				setCdlCategory(category?.value)
+			})
 
+			toast.success(t("successfully_saved_information"));
+		}
 	}, [])
+
+
+	const categoryTypes = cdlCategory.join(', ')
 	return (
 		<>
 			<ToastContainer />
@@ -35,9 +52,11 @@ export function ContinueLongForm() {
 					</h4>
 				</Row>
 				<Row className="mt-3">
-					<h6 className={`${styles.paragraph} ${styles.margin__top}`}>
-						{t("{company_name}_THANKS_NOTE", { company_name: applicant?.company?.name }, { translateProps: true })}
-					</h6>
+					{cdlCategory.length > 0 && !cdlCategory.includes(applicant?.license_type) && (
+						<h6 className={`${styles.paragraph} ${styles.margin__top} bg-danger text-light  p-1`}>
+							{t("{company_name}_THANKS_NOTE_{cdl_category}", { company_name: applicant?.company?.name, cdl_category: categoryTypes }, { translateProps: true })}
+						</h6>
+					)}
 				</Row>
 				<Row className="mt-3">
 					<Col className="text-center" >
