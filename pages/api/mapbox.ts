0080336689da0681d24df6cbc0043a43240dd857
talhabhuttacodeup <talha.bhutta@codeupscale.com>
2023-02-02 14:26:07 +0500

    DRIV-1014 | comment added for bbox description

--- pages/api/mapbox.ts
+++ pages/api/mapbox.ts
@@ -5,6 +5,7 @@ export default class MapboxApi extends BaseApi {
     }
 
     async forwardGeocoding(query: string, endpoint = "mapbox.places") {
+        // coordinates to specify the location of USA
         const bbox = "-125.0011,24.9493,-66.9326,49.5904";
         const baseUrl = `https://api.mapbox.com/geocoding/v5/${endpoint}/${query}.json?access_token=${process.env.MAPBOX_API_KEY}&autocomplete=true&bbox=${bbox}`
         const { data } = await this.get(baseUrl);
