36b2565002b27fe7aeb8591c2994fb4d7be1c6af
Khuram K <khurramk@codeupscale.com>
2023-02-03 16:18:37 +0500

    DRIV-1068 | code optimization and fixes

--- models/jot-form/long-form/violation-history.dto.ts
+++ models/jot-form/long-form/violation-history.dto.ts
@@ -2,14 +2,21 @@ import * as yup from "yup";
 import { ApplicantExtrasEntity } from "../../applicant/applicant-extras.entity";
 
 export class ViolationHistoryDto {
-	VIOLATION_COUNT?: ApplicantExtrasEntity;
 	VIOLATION_DETAILS?: ApplicantExtrasEntity;
 	moving_violations_count?: Number;
 	static yupSchema() {
 		return yup.object({
-			VIOLATION_COUNT: ApplicantExtrasEntity.yupSchema(),
 			VIOLATION_DETAILS: ApplicantExtrasEntity.yupSchema(),
-			moving_violations_count: yup.number().optional().nullable(),
+			moving_violations_count: yup.number()
+				.required()
+				.when(
+					'VIOLATION_DETAILS',
+					(VIOLATION_DETAILS: ApplicantExtrasEntity, schema) =>
+						schema.min(VIOLATION_DETAILS?.value?.length ?? 0)
+							.required()
+							.nullable()
+				)
+				.nullable(),
 		});
 	}
 }
