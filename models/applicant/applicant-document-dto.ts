9056a965bb4fa86fc0a5c9a0d140b5fe00cd1331
talhabhuttacodeup <talha.bhutta@codeupscale.com>
2023-04-27 20:05:57 +0500

    DRIV-1129 | bug resolved

--- models/applicant/applicant-document-dto.ts
+++ models/applicant/applicant-document-dto.ts
@@ -1,9 +1,5 @@
 import { DocumentEntity } from '../documents/document.entity';
 import * as yup from "yup";
-// import { ApplicantDocumentType } from '../../enums/applicants/applicant-document-type.enum';
-import { ApplicantDqf } from '../../enums/applicants/applicant-dqf-types.enum';
-import { ApplicantOnBoardingChecklist } from '../../enums/applicants/applicant-onboarding-checklist.enum';
-
 export class ApplicantDocumentDto {
     document?: DocumentEntity;
 
