106013f4c15b1eab33c075b8004ed6b180251f14
Khuram K <khurramk@codeupscale.com>
2023-04-28 20:01:27 +0500

    DRIV-1117 | debugging

--- components/messenger/messenger.tsx
+++ components/messenger/messenger.tsx
@@ -154,7 +154,7 @@ export function Messenger(props) {
         argument will be executed. In this case, it simply logs a message to the console indicating that
         there was a connection error and the reason for the error. */
         socket.on("connect_error", (err) => {
-            console.log(`Socket :: connect_error due to ${err.message}`);
+            console.log(`Socket :: connect_error due to ${err.message}`, err);
         });
 
         /* Listening for a message from the server, and when it receives a message, it finds the conversation
