46ac8bfb48e84bc227039e5029487722f704429b
Khuram K <khurramk@codeupscale.com>
2023-07-04 14:58:17 +0500

    DRIV-1161 fixes

--- components/forms/jotform/shortForm/hear-about.tsx
+++ components/forms/jotform/shortForm/hear-about.tsx
@@ -19,7 +19,7 @@ import { LoaderIcon } from "../../../loading/loader-icon";
 export function HearAbout() {
 	const {
 		state: { applicantExtras, applicant },
-		method: { updateApplicantExtras, stepNext, stepBack, setApplicant },
+		method: { updateApplicantExtras, setApplicantExtras, stepNext, stepBack, setApplicant },
 	}: JotFormContextType = useContext(JotformContext);
 
 	const { t } = useTranslation();
@@ -30,21 +30,19 @@ export function HearAbout() {
 		onSubmit: async (values) => {
 			const applicantApi = new ApplicantApi();
 			const { HEAR_ABOUT_US, REFERAL_NAME } = values;
-			updateApplicantExtras(HEAR_ABOUT_US);
-			updateApplicantExtras(REFERAL_NAME);
 			if (applicant?.can_pass_drug_test) {
 				try {
-
 					const filteredExtras = ([
 						...applicantExtras,
 						{ ...HEAR_ABOUT_US },
-						(Boolean(REFERAL_NAME?.value) ? { ...REFERAL_NAME } : null)
+						{ ...REFERAL_NAME },
 					]).filter(v => !!v?.value)
 
 					const data = await applicantApi.jotform.create({
 						applicant,
 						applicantExtras: filteredExtras,
 					});
+					setApplicantExtras(filteredExtras)
 					setApplicant({
 						...applicant,
 						...data,
@@ -72,7 +70,7 @@ export function HearAbout() {
 		const apx_referal_name = applicantExtras?.find(
 			(v) => v.type === ApplicantExtras.REFERAL_NAME
 		);
-		
+
 		form.setValues({
 			...form.values,
 			HEAR_ABOUT_US: !!apx?.type
@@ -83,13 +81,15 @@ export function HearAbout() {
 				: new ApplicantExtrasEntity(ApplicantExtras.REFERAL_NAME)
 		});
 	}, [applicantExtras]);
-	useEffect(() => {
-		console.log("form.values===", form?.values)
-		console.log("form.errors===", form?.errors)
-	}, [form?.values, form?.errors])
+
+	// Uncomment code below in debugging mode
+	// useEffect(() => {
+	// 	console.log("form.values===", form?.values)
+	// 	console.log("form.errors===", form?.errors)
+	// }, [form?.values, form?.errors])
+
 	return (
 		<>
-
 			<form onSubmit={form.handleSubmit} onReset={form.handleReset}>
 				<Row>
 					<h4 className={styles.heading__sty}>
--- models/jot-form/short-form/hear-about.dto.ts
+++ models/jot-form/short-form/hear-about.dto.ts
@@ -3,16 +3,16 @@ import { ApplicantExtrasEntity } from "../../applicant/applicant-extras.entity";
 import { HearAboutUsType } from "../../../enums/jotform/hear-about-type.enum";
 
 export class HearAboutUsDto {
-  HEAR_ABOUT_US: ApplicantExtrasEntity;
-  REFERAL_NAME?: ApplicantExtrasEntity
+	HEAR_ABOUT_US: ApplicantExtrasEntity;
+	REFERAL_NAME?: ApplicantExtrasEntity
 
-  static yupSchema() {
-    return yup.object({
-      HEAR_ABOUT_US: ApplicantExtrasEntity.yupSchema(),
-      REFERAL_NAME: yup.object().when("HEAR_ABOUT_US", {
-        is: v => v == HearAboutUsType.REFERRAL,
-        then: ApplicantExtrasEntity.yupSchema()
-      }).nullable()
-    });
-  }
+	static yupSchema() {
+		return yup.object({
+			HEAR_ABOUT_US: ApplicantExtrasEntity.yupSchema(),
+			REFERAL_NAME: yup.object().when("HEAR_ABOUT_US", {
+				is: v => v.value == HearAboutUsType.REFERRAL,
+				then: ApplicantExtrasEntity.yupSchema()
+			}).nullable()
+		});
+	}
 }
