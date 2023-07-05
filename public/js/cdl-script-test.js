77bedd4e889353dfbe38d95e3493ad57792d0b5f
Khuram K <khurramk@codeupscale.com>
2023-07-05 17:22:56 +0500

    cdl-script added in public

---
+++ public/js/cdl-script-test.js
@@ -0,0 +1,6 @@
+alert()
+const type = document.currentScript.getAttribute('filterType');
+var iframe = document.createElement('iframe');
+let urlString = iframe.src = `https://test.driverfly.co/embedded?filterType=${type}`;
+iframe.style = 'width: 100 %; height: 99vh; border: none;'
+document.body.appendChild(iframe);
