679435de3f619148d5e4b88d7d167ee752a2dc28
talhabhuttacodeup <talha.bhutta@codeupscale.com>
2023-07-20 18:01:08 +0500

    DRIV-1344 | removeeed console to pmeerege codeee

--- components/forms/jotform/shortForm/continue-longform.tsx
+++ components/forms/jotform/shortForm/continue-longform.tsx
@@ -80,7 +80,6 @@ export function ContinueLongForm() {
 	}
 
 	const hasJobGeographyInRouteType = checkJobGeographyInRouteType(ApplicantAddedRoutes?.value, CompanyPreferedLocations?.value);
-	console.log("applicant?", applicant)
 	return (
 		<>
 			<ToastContainer />
