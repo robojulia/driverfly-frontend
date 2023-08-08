cf43b61c19698bec91796f14579daee32aee5a04
Khuram K <khurramk@codeupscale.com>
2023-08-08 17:48:52 +0500

    DRIV-1360 | employee experience fixes

---
+++ models/applicant/employee-experience.entity.ts
@@ -0,0 +1,28 @@
+import * as yup from "yup";
+import "../../utils/yup";
+import { JobEquipmentType } from "../../enums/jobs/job-equipment-type.enum";
+
+export class EmployeeExperienceEntity {
+    type?: JobEquipmentType;
+    type_other?: string;
+    years?: number;
+    months?: number;
+
+    static yupSchema() {
+        return yup.object({
+            type: (yup.string() as any).enum(JobEquipmentType).required().nullable(),
+            type_other: yup.string().when("type", {
+                is: JobEquipmentType.OTHER,
+                then: yup.string().required().nullable()
+            }).nullable(),
+            years: yup.number().min(1).nullable(),
+            months: yup.number().min(0).max(11).nullable()
+        });
+    }
+
+    static key(entity: EmployeeExperienceEntity) {
+        if (entity.type === JobEquipmentType.OTHER) return `${entity.type}_${entity.type_other}`;
+
+        return entity.type;
+    }
+}
--- models/employee/employee.entity.ts
+++ models/employee/employee.entity.ts
@@ -14,6 +14,7 @@ import { DocumentEntity } from '../documents/document.entity';
 import { EmployeeStatus } from '../../enums/applicants/employee-status.enum';
 import { CompanyEntity } from '../company/company.entity';
 import { CompanyManagerEntity } from '../company/company-manager.entity';
+import { EmployeeExperienceEntity } from './employee-experience.entity';
 
 export class EmployeeEntity {
 	id?: number;
@@ -48,7 +49,7 @@ export class EmployeeEntity {
 	highest_degree?: string;
 	authorized_to_work_in_us?: boolean;
 	preferred_location?: string[];
-	equipment_experience?: ApplicantExperienceEntity[];
+	equipment_experience?: EmployeeExperienceEntity[];
 	transmission_type?: string[];
 	endorsements?: string[];
 	emergency_contact_name?: string;
@@ -125,8 +126,8 @@ export class EmployeeEntity {
 			// 	then: yup.string().required().nullable(),
 			// }).nullable(),
 			equipment_experience: (
-				yup.array(ApplicantExperienceEntity.yupSchema()) as any
-			).unique("type", { mapper: ApplicantExperienceEntity.key }),
+				yup.array(EmployeeExperienceEntity.yupSchema()) as any
+			).unique("type", { mapper: EmployeeExperienceEntity.key }),
 			assignedUserId: yup.number().optional().nullable()
 		});
 	}
