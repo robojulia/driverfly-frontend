9fdd34b106edd4a351dcd4db6edc4fdd71fbdee2
Ukkasha <muhammad.ukkasha@codeupscale.com>
2023-09-26 12:38:54 +0500

    DRIV-1455 | Title Field Error

--- models/jot-form/long-form/past-employment-history/index.dto.ts
+++ models/jot-form/long-form/past-employment-history/index.dto.ts
@@ -11,6 +11,7 @@ export class PastEmploymentHistoryDto extends ApplicantEmployerEntity {
 	static derivedYupSchema() {
 		return yup.object({
 			name: yup.string().required().trim().nullable(),
+			title: yup.string().required().trim().nullable(),
 			manager_name: yup.string().required().trim().nullable(),
 			phone: yup.string().optional().nullable(),
 			city: yup.string().required().trim().nullable()
