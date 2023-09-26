d834fb6e4651db2e2caa6451b44feca46a85c317
Ukkasha <muhammad.ukkasha@codeupscale.com>
2023-09-26 13:35:03 +0500

    DRIV-1424 | Invisible Text on VOE

--- components/forms/jotform/voe-forms/submission-details.tsx
+++ components/forms/jotform/voe-forms/submission-details.tsx
@@ -106,7 +106,7 @@ export function SubmissionDetails() {
 			<Form onSubmit={form.handleSubmit} onReset={form.handleReset}>
 				<Row className={`${styles.align__text_left}`}>
 					<Col md="10">
-						<h6 className={styles.bold}>{t("SIGNATURE")}</h6>
+						<h6 className={`${styles.bold} text-black ${styles.bold}`}>{t("SIGNATURE")}</h6>
 						<SignatureCanvas
 							name="SIGNATURE_VOE.value"
 							ref={padRef}
