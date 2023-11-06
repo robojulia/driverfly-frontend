bc1929c27c06c460f31ee531cb12ca0c5dff3135
Ukkasha <muhammad.ukkasha@codeupscale.com>
2023-11-06 12:15:05 +0500

    DRIV-1555 | Toast Text Changed

--- components/forms/base-re-capcha.tsx
+++ components/forms/base-re-capcha.tsx
@@ -24,6 +24,7 @@ function BaseReCapcha({
   onChange,
 }: BaseReCapchaProps) {
   const { t } = useTranslation();
+  const key = '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'
 
   if (formik) {
     /**
@@ -46,7 +47,7 @@ function BaseReCapcha({
       )}
       <ReCAPTCHA
         className={`${error? "is-invalid" : ""}`}
-        sitekey={process.env.RECAPTCHA_SITE_KEY}
+        sitekey={key}
         onChange={onChange}
         ref={captchaRef}
       />
