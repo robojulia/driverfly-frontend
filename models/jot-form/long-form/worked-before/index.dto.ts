4980bd5374989d371f30dc6ffa57519e93d2f13d
Ukkasha <muhammad.ukkasha@codeupscale.com>
2024-01-23 16:41:13 +0500

    DRIV-1558 | End date after start date

--- models/jot-form/long-form/worked-before/index.dto.ts
+++ models/jot-form/long-form/worked-before/index.dto.ts
@@ -2,24 +2,24 @@ import * as yup from "yup";
 import "../../../../utils/yup";
 
 export class WorkedBeforeExtrasDto {
-  start_date: string | Date
-  end_date: string | Date
-  static yupSchema() {
-    return yup.object({
-        start_date: yup.date().max(new Date()).required().nullable(),
-        end_date: yup.date().test({
-					test: (value, context) => {
-						const start_date = context.resolve(yup.ref('start_date'));
-            if(Boolean(start_date)) return true;
-						if (!Boolean(value)) return true;
-						if (Boolean(value > start_date)) return true;
+	start_date: string | Date
+	end_date: string | Date
+	static yupSchema() {
+		return yup.object({
+			start_date: yup.date().max(new Date()).required().nullable(),
+			end_date: yup.date()
+			.test({
+				test: (value, context) => {
+					const start_date = context.resolve(yup.ref('start_date'));
+					if (!Boolean(value)) return true;
+					if (value > start_date) return true;
 
-						return context.createError({
-							path: context.path,
-							message: 'END_DATE_MUST_BE_AFTER_START_DATE'
-						})
-					}
-				}).required().nullable(),
-    });
-  }
+					return context.createError({
+						path: context.path,
+						message: 'END_DATE_MUST_BE_AFTER_START_DATE'
+					})
+				}
+			}).required().nullable(),
+		});
+	}
 }
