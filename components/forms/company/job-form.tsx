0d458115c45b05f13534dee0c5f7589e4d62cf46
Khuram K <khurramk@codeupscale.com>
2024-02-05 17:12:44 +0500

    Hot fixes

--- components/forms/company/job-form.tsx
+++ components/forms/company/job-form.tsx
@@ -92,8 +92,8 @@ export function JobForm(props: JobFormProps) {
             ...(entity || {}),
             schedule: entity?.schedule || JobSchedule.OPEN_TO_NEGOTIATE,
             pay_method: entity?.pay_method || JobPayMethod.OPEN_TO_NEGOTIATE,
-            min_weekly_pay: Boolean(entity.min_weekly_pay) ? entity.min_weekly_pay : null,
-            max_weekly_pay: Boolean(entity.max_weekly_pay) ? entity.max_weekly_pay : null,
+            min_weekly_pay: Boolean(entity?.min_weekly_pay) ? entity.min_weekly_pay : null,
+            max_weekly_pay: Boolean(entity?.max_weekly_pay) ? entity.max_weekly_pay : null,
         });
     }, [entity]);
 
