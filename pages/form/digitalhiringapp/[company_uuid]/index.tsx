085c572a4128d867702dc616ff27d874f21e10bc
Khuram K <khurramk@codeupscale.com>
2024-02-05 18:30:28 +0500

    Hot fixes

--- pages/form/digitalhiringapp/[company_uuid]/index.tsx
+++ pages/form/digitalhiringapp/[company_uuid]/index.tsx
@@ -87,7 +87,7 @@ export async function getServerSideProps({ query }: NextPageContext) {
 	try {
 		let companyId = String(query?.company_uuid); // { companyId } = query || {};
 		console.log("companyId", companyId);
-		
+
 
 		const utm: UtmReferral = {
 			utm_source: query?.utm_source as string ?? null,
@@ -106,7 +106,7 @@ export async function getServerSideProps({ query }: NextPageContext) {
 		const employer: CompanyEntity = await companyApi.employer.getByUUId(companyId);
 		const preferences: CompanyPreferenceEntity[] = await companyApi.preferences.list(employer.id)
 
-		if (employer?.status !== Status.ACTIVE) {
+		if (employer?.status != Status.ACTIVE) {
 			if (employer == null) {
 				console.error(`form/jotform: Employer ${query?.company_uuid} not found - does not exist`);
 			} else {
