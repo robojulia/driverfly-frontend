0ac7362ea0f74838fdfa7c41e02072629f6ab655
Ukkasha <muhammad.ukkasha@codeupscale.com>
2024-02-06 15:03:49 +0500

    DRIV-1672 | Toast changes

--- pages/dashboard/company/company-preferences/index.tsx
+++ pages/dashboard/company/company-preferences/index.tsx
@@ -142,7 +142,7 @@ export default function CompanyPreference() {
 				);
 				setPreferences([...preferences, ...data]);
 				populateForm(data);
-				toast.success(t("successfully_saved_information"));
+				toast.success(t("successfully_updated_company_preferences"));
 			} catch (e) {
 				console.error("Unable to save preferences", e);
 				toast.error(t("unable_to_save_information"));
@@ -206,7 +206,7 @@ export default function CompanyPreference() {
 				value: !pref.value,
 			});
 		} else {
-			pref = await api.preferences.create(user?.company?.id, {
+			pref = await api.preferences.create(user?.company?.id, {	
 				category: CompanyPreferenceCategory.AUTO_RECRUITING,
 				label,
 				value: true,
