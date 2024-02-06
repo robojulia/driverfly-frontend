99e74d0bfe2e075075ef129bfd6361d4cb0149a2
Ukkasha <muhammad.ukkasha@codeupscale.com>
2024-02-06 13:02:27 +0500

    DRIV-1601 | seprated code of another ticket

--- components/news-letter-signup/index.tsx
+++ components/news-letter-signup/index.tsx
@@ -8,43 +8,23 @@ import BaseInput from '../forms/base-input';
 export default function NewsletterSingup() {
 
     const { t } = useTranslation();
-    const form = useFormik({
-        initialValues: {
-            email: ''
-        },
-        validationSchema: yup.object().shape({
-            email: yup.string().email('Invalid email address').trim().nullable(),
-        }),
-        onSubmit: async (values) => {
-            console.log(values, "Email for news letter..............");
-            alert(values)
-        }
-    })
+
 
     return (
         <>
-            <div className=' d-flex flex-column align-items-center justify-content-center'>
-                <h1 className='general-headings text-white mt-3 text-uppercase'>{t("NEWSLETTER_SIGN_UP")}</h1>
-                <p>{t("SUBSCRIBE_TO_DRIVERFLY_PACIFIC_NEWALETTERTO_GET_THE_LATEST_JOBS_POSTED")}<br />{t("CANDIDATES_AND_OTHER_LATEST_NEWS")} ​</p>
-                <div className=" my-3 w-50" >
-                    <form onSubmit={form.handleSubmit} className=' w-100 ml-5 d-flex'>
-                        {/* <div className="input-group input-group-lg"> */}
-                        <BaseInput
-                            className=" w-75   "
-                            name="email"
-                            placeholder="Enter Your Email"
-                            type="text"
-                            formik={form}
-                        />
-                        {/* <input type="text" className="form-control" placeholder='Enter Your Email' /> */}
-                        <button type='submit' className="p-3 newletter-btn"
-                            style={{ backgroundColor: "#2EC8C4", borderColor: "#2EC8C4" }}>
-                            <ArrowRight color='#fff' />
-                        </button>
-                        {/* </div> */}
-                    </form>
+        <div className=' d-flex flex-column align-items-center justify-content-center'>
+            <h1 className='general-headings text-white mt-3 text-uppercase'>{t("NEWSLETTER_SIGN_UP")}</h1>
+            <p>{t("SUBSCRIBE_TO_DRIVERFLY_PACIFIC_NEWALETTERTO_GET_THE_LATEST_JOBS_POSTED")}<br />{t("CANDIDATES_AND_OTHER_LATEST_NEWS")} ​</p>
+            <div className="input-group my-3 w-50" >
+                <div className="input-group input-group-lg">
+                    <input type="text" className="form-control" placeholder='Enter Your Email' />
+                    <span className="input-group-text" id="inputGroup-sizing-lg"
+                        style={{ backgroundColor: "#2EC8C4", borderColor: "#2EC8C4" }}>
+                        <ArrowRight color='#fff' />
+                    </span>
                 </div>
             </div>
-        </>
+        </div>
+    </>
     )
 }
