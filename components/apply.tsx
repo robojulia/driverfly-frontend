6950f3fc299edcf6fa6c3a8ab432e7113a1b3a6b
Khuram K <khurramk@codeupscale.com>
2024-02-19 15:58:37 +0500

    DRIV-1654 | wip

---
+++ components/apply.tsx
@@ -0,0 +1,360 @@
+import { useFormik } from "formik"
+import { useEffect, useState } from 'react'
+import { Col, Row } from 'react-bootstrap'
+import { ArrowRight, DashCircle, PlusCircle } from 'react-bootstrap-icons'
+import Button from "react-bootstrap/Button"
+import { toast } from 'react-toastify'
+import { ApplicantDocumentType } from '../enums/applicants/applicant-document-type.enum'
+import { DriverLicenseType } from "../enums/users/driver-license-type.enum"
+import { EducationLevel } from '../enums/users/education-level.enum'
+import { SharePreference } from '../enums/users/share-preference.enum'
+import { UserPreferenceCategory } from '../enums/users/user-preference-category.enum'
+import { useAuth } from '../hooks/use-auth'
+import { useTranslation } from "../hooks/use-translation"
+import { ApplicantEntity } from '../models/applicant/applicant.entity'
+import { DocumentEntity } from '../models/documents/document.entity'
+import ApplicantApi from '../pages/api/applicant'
+import JobApi from '../pages/api/job'
+import UserApi from '../pages/api/user'
+import { globalAjaxExceptionHandler } from "../utils/ajax"
+import { useEffectAsync } from '../utils/react'
+import BaseCheck from './forms/base-check'
+import BaseInput from './forms/base-input'
+import BaseInputPhone from './forms/base-input-phone'
+import BaseSelect from './forms/base-select'
+import FileInput from "./forms/file-input"
+import { LoaderIcon } from './loading/loader-icon'
+import ViewCard from './view-details/view-card'
+import ViewModal from "./view-details/view-modal"
+
+export default function JobApply({ job, setEncourageModal }) {
+    const { user } = useAuth();
+    const { t } = useTranslation();
+    const jobApi = new JobApi();
+    const applicantApi = new ApplicantApi();
+    const userApi = new UserApi();
+
+    const [showModal, setShowModal] = useState(false);
+    const [hideForm, setHideForm] = useState(false);
+
+    const apply_form = useFormik({
+        initialValues: new ApplicantEntity(),
+        validationSchema: ApplicantEntity.yupSchemaForApplyForm(),
+        onSubmit: async (dto, { resetForm }) => {
+            dto.years_cdl_experience = Number(dto.years_cdl_experience);
+            dto.moving_violations_count = Number(dto.moving_violations_count);
+            dto.accident_count = Number(dto.accident_count);
+
+            try {
+                const applicant = await jobApi.apply(job.id, dto);
+
+                toast.success(t('job_applied_success_message'))
+                setShowModal(false);
+                // resetForm()
+                setEncourageModal(true)
+            }
+            catch (e) {
+                globalAjaxExceptionHandler(e, { formik: apply_form, toast: toast, t: t });
+                if (e.response?.data?.message == "ApplicantJobService.APPLICANT_ALREADY_APPLIED") setShowModal(false)
+            }
+        }
+    });
+
+    useEffectAsync(async () => {
+        let applicant = new ApplicantEntity();
+        if (user && user.id) {
+            try {
+                if (!user.company) {
+                    applicant = await applicantApi.me.get();
+                    applicant.documents = applicant.documents.filter(v => Object.values(ApplicantDocumentType).includes(v.type as ApplicantDocumentType))
+
+                    if (applicant) {
+                        const preferences = await userApi.preferences.list(user.id, { category: UserPreferenceCategory.SHARING });
+
+                        if (preferences.length > 0) {
+                            applicant.documents = applicant.documents.filter(
+                                (document) => !preferences.some(
+                                    (preference) => preference.label == document.type && preference.value == SharePreference.NEVER
+                                )
+                            );
+                        } else applicant.documents = applicant.documents?.filter((document) => document.type == ApplicantDocumentType.RESUME);
+
+                    }
+                }
+            }
+            catch (e) {
+                if (e.response?.status == 401) {
+                    return;
+                }
+                throw e;
+            }
+        } else {
+            applicant.years_cdl_experience = 0;
+            applicant.moving_violations_count = 0;
+            applicant.accident_count = 0;
+        }
+        console.log({ applicant });
+
+        apply_form.setValues({
+            ...apply_form.values,
+            ...applicant,
+        });
+    }, [showModal])
+    const [showForm, setShowForm] = useState(false);
+
+    const onApplyClick = (e) => {
+        setShowModal(!showModal);
+        const [showForm, setShowForm] = useState(false);
+    }
+
+    const onCloseClick = () => {
+        // apply_form.resetForm()
+        setShowModal(false);
+    }
+
+    useEffect(() => {
+        console.log("values", apply_form.values);
+        console.log("errors", apply_form.errors);
+    }, [apply_form.values, apply_form.errors])
+
+    return (
+        <>
+            <div className="ort-btn mt-lg-4 mt-0">
+                <button type="button" className="btn theme-primary-btn" onClick={onApplyClick}> {t('APPLY_NOW')}<ArrowRight /></button>
+            </div>
+            <ViewModal
+                size={"xl"}
+                show={showModal}
+                const [showForm, setShowForm] = useState(false);
+            closeText="CANCEL"
+            onCloseClick={onCloseClick}
+            title="apply_for_this_job"
+            footer={
+                <button
+                    disabled={!!apply_form.isSubmitting || !!!apply_form.isValid || !!apply_form.isValidating}
+                    type="submit"
+                    className="btn btn-primary w-100 p-lg-3 p-5"
+                    onClick={() => apply_form.handleSubmit()}>
+                    <LoaderIcon isLoading={!!apply_form.isSubmitting} /> {t('submit')}
+                </button>
+            }
+            >
+            <form onSubmit={apply_form.handleSubmit}>
+                {/* {typeof apply_form.errors.job == "string" &&
+                        <Row>
+                            <span className='text-danger'>{apply_form.errors.job}</span>
+                        </Row>
+                    } */}
+                <Row>
+                    <BaseInput
+                        className=" col-md-6 mt-3"
+                        label="first_name"
+                        placeholder="first_name"
+                        name="first_name"
+                        required
+                        formik={apply_form}
+                    />
+                    <BaseInput
+                        className="col-md-6 mt-3"
+                        label="last_name"
+                        placeholder="last_name"
+                        name="last_name"
+                        required
+                        formik={apply_form}
+                    />
+                </Row>
+                <Row>
+                    <BaseInput
+                        readOnly={user && !user.company ? true : false}
+                        required
+                        type="email"
+                        className=" col-md-6 mt-3"
+                        label="email"
+                        placeholder="email"
+                        name="email"
+                        formik={user?.company ? null : apply_form}
+                    />
+                    <BaseInputPhone
+                        className=" col-md-6 mt-3"
+                        label="contact_number"
+                        placeholder="contact_number"
+                        name="phone"
+                        formik={apply_form}
+                    />
+                </Row>
+                <Row>
+                    <BaseSelect
+                        className="col-6 mt-3"
+                        label="highest_degree"
+                        placeholder="highest_degree"
+                        name="highest_degree"
+                        enumType={EducationLevel}
+                        labelPrefix="EducationLevel"
+                        formik={apply_form}
+                    />
+                    <BaseInput
+                        className="col-6 mt-3 no-arrow"
+                        name="zip_code"
+                        type="number"
+                        label="zip_code"
+                        placeholder="zip_code"
+                        formik={apply_form}
+                    />
+                </Row>
+                <Row className="mt-3">
+                    <BaseSelect
+                        className="col-6 "
+                        label="CDL_CLASS"
+                        name="license_type"
+                        // placeholder="DriverLicenseType.NONE"
+                        labelPrefix="DriverLicenseType"
+                        enumType={DriverLicenseType}
+                        formik={apply_form}
+                        onChange={(e) => {
+                            apply_form.handleChange(e)
+                            if (![DriverLicenseType.CDL_CLASS_A, DriverLicenseType.CDL_CLASS_B].includes(apply_form.values?.license_type)) {
+                                apply_form.setFieldValue("years_cdl_experience", 0)
+                            }
+                        }}
+                    />
+                    {!!apply_form.values.license_type && apply_form.values.license_type !== DriverLicenseType.NO_CDL && (
+                        <>
+                            <BaseInput
+                                className="col-6"
+                                type="number"
+                                step={0.1}
+                                min={0}
+                                name="years_cdl_experience"
+                                label="years_cdl_experience"
+                                placeholder="PLACEHOLDER_FOR_DIGITS"
+                                required
+                                formik={apply_form}
+                            />
+                        </>
+                    )}
+                </Row>
+                {[DriverLicenseType.CDL_CLASS_A, DriverLicenseType.CDL_CLASS_B].includes(apply_form.values?.license_type) && (
+                    <Row className="mt-3 ">
+                        <BaseCheck
+                            className="my-3"
+                            name="is_owner_operator"
+                            label="is_owner_operator_question"
+                            formik={apply_form}
+                        />
+                    </Row>
+                )}
+                {/* Files Start */}
+                <Row>
+                    <Col sm="12" className="mt-3">
+                        <ViewCard
+                            title="DOCUMENTS"
+                            actions={<Button size='sm'
+                                disabled={apply_form.values.documents?.length == Object.keys(ApplicantDocumentType).length}
+                                onClick={() => apply_form.setValues({
+                                    ...apply_form.values,
+                                    documents: [
+                                        ...(apply_form.values.documents || []),
+                                        new DocumentEntity()
+                                    ]
+                                })}><PlusCircle /> {t("ADD")}</Button>}
+                        >
+                            {!apply_form.values.documents?.length &&
+                                t("NONE")
+                            }
+                            {
+                                apply_form.values.documents?.length > 0 &&
+                                <>
+                                    <Row className="p-1">
+                                        {
+                                            apply_form.values
+                                                .documents
+                                                .map((entity, i) => (
+                                                    <Row key={i} className="pr-0">
+                                                        <Col className='my-2 pr-0'>
+                                                            <BaseSelect
+                                                                name={`documents[${i}].type`}
+                                                                required
+                                                                placeholder="TYPE"
+                                                                labelPrefix="ApplicantDocumentType"
+                                                                enumType={ApplicantDocumentType}
+                                                                readOnly={!!entity.id && !entity.file_base64}
+                                                                formik={apply_form}
+                                                            />
+                                                        </Col>
+                                                        <Col md="6" className="pr-0 my-2">
+                                                            <FileInput
+                                                                allowedSizeInByte={3145728}
+                                                                name={`documents[${i}]`}
+                                                                required
+                                                                accept="application/pdf"
+                                                                formik={apply_form}
+                                                            />
+                                                        </Col>
+                                                        <div className="col-md-1 mt-lg-3 mt-md-3 mt-1 text-right p-0">
+                                                            <button onClick={() => apply_form.setValues({
+                                                                ...apply_form.values,
+                                                                documents: apply_form.values.documents.filter((v, idx) => i != idx)
+                                                            })}><DashCircle color="red" /></button>
+                                                        </div>
+                                                    </Row>
+                                                ))}
+                                    </Row>
+
+                                </>
+                            }
+                        </ViewCard>
+                    </Col>
+                </Row>
+                {/* Files End */}
+
+                <Row>
+                    <BaseInput
+                        className="col my-3"
+                        required
+                        name="moving_violations_count"
+                        type="number"
+                        step={1}
+                        min={0}
+                        label="voilations_in_last_3_years"
+                        placeholder="PLACEHOLDER_FOR_DIGITS"
+                        formik={apply_form}
+                    />
+                    <BaseInput
+                        className="col my-3"
+                        required
+                        name="accident_count"
+                        type="number"
+                        step={1}
+                        min={0}
+                        label="accidents_last_5_years"
+                        placeholder="PLACEHOLDER_FOR_DIGITS"
+                        formik={apply_form}
+                    />
+                </Row>
+
+                <Row>
+                    <BaseCheck
+                        className="col-md-6 mt-4 mb-1"
+                        label="can_pass_drug_and_alcohol_test"
+                        name="can_pass_drug_test"
+                        formik={apply_form}
+                    />
+                </Row>
+                <Row >
+                    <p className={`text-bold`}>
+                        {t("{company_name}_DRUG_TEST_DOT", { company_name: job?.company?.name }, { translateProps: true })}
+                    </p>
+                </Row>
+                <Row>
+                    <div className="  col-12 p-0">
+                        <p className='mx-3 mt-5'>{t('i_accept_{terms_and_condition}', { terms_and_condition: t('terms_and_condition') })}</p>
+                    </div>
+                </Row>
+            </form>
+
+
+        </ViewModal >
+
+        </>
+    )
+}
