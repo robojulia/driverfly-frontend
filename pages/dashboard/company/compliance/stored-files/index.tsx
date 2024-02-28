bf5f6ec925fa5dd31d4648cb89e3ad627146640b
Ukkasha <muhammad.ukkasha@codeupscale.com>
2024-02-28 18:18:04 +0500

    DRIV-1395 | Removing Strored Files

--- pages/dashboard/company/compliance/stored-files/index.tsx
+++ pages/dashboard/company/compliance/stored-files/index.tsx
@@ -34,6 +34,7 @@ import ApplicantApi from "../../../../api/applicant";
 import ComplianceApi from "../../../../api/compliance";
 import EmployeeApi from "../../../../api/employee";
 import { DeleteDocumentButton } from "../../../../../components/documents/buttons";
+import { LoaderIcon } from "../../../../../components/loading/loader-icon";
 
 export default function StoredFiles() {
     const { user } = useAuth();
@@ -41,15 +42,22 @@ export default function StoredFiles() {
     const complianceApi = new ComplianceApi();
     const applicantApi = new ApplicantApi();
     const employeeApi = new EmployeeApi();
-
+    
     // showFileUploadModel
-    const [showFileUploadModel, setShowFileUploadModel] =
-        useState<boolean>(false);
+    const [showFileUploadModel, setShowFileUploadModel] =  useState<boolean>(false);
+    const [confirmationModal, setConfirmationModal] = useState<any>({
+        value:false,
+        fileId : null,
+    });
+    const [documentId, setDocumentId] = useState<number>(null);
+
     const openFileUploadModel = (): void => setShowFileUploadModel(true);
     const closeFileUploadModel = (): void => setShowFileUploadModel(false);
-
-    const [documentId, setDocumentId] = useState<number>(null);
+    
     const resetDocumentId = (): void => setDocumentId(null);
+    // const confirmationModalToggle = (id) => setConfirmationModal({value : !confirmationModal.value, fileId : id ? id : null});
+
+    console.log("confirmationModal values : => ", confirmationModal);
 
     const columnSettingKey = getDataTableColumnKey(
         "company",
@@ -79,15 +87,11 @@ export default function StoredFiles() {
         }
     );
 
-    console.log("Files : => ",files);
-    console.log("applicants : => ",applicants);
-
     const handleDeleteFile = async (fileId) => {
-        const res = await complianceApi.removeFile(fileId);
+        await complianceApi.removeFile(fileId);
         setFiles(files?.filter( itm => itm.id !== fileId))
-        console.log("Response from Delete File => : ", fileId);
-        console.log("Response from res => : ", res);
-        // setFiles(f);
+        setConfirmationModal({value : !confirmationModal.value, fileId: null})
+        toast.success(t("DOCUMENT_DELETED_SUCCESS_MESSAGE"))
     }
     const form = useFormik({
         initialValues: new StoredFileDto(),
@@ -415,10 +419,10 @@ export default function StoredFiles() {
                                         <Eye />
                                     </button>
                                 )}
-                                <div className="p-2">
+                                <div className="p-2 ">
                                     <button className="btn btn-danger py-1 px-3 "
                                         type="button"
-                                        onClick={() => handleDeleteFile(file.id)}
+                                        onClick={()=>setConfirmationModal({value : !confirmationModal.value, fileId: file.id})}
                                     ><Trash /></button>
                                 </div>
                             </>
@@ -463,9 +467,23 @@ export default function StoredFiles() {
                     </Row>
                 </EntityForm>
             </ViewModal>
+            {/* Modal for deleting document */}
+            <ViewModal
+                title="CONFIRMATION"
+                show={confirmationModal.value}
+                onCloseClick={()=>setConfirmationModal({value : !confirmationModal?.value, fileId: null})}
+                size='sm'
+            >
+                <div className="d-flex flex-column align-items-center justify-content-between w-100 gap-5">
+                    <h2>{t("ARE_YOU_SURE_YOU_WANT_TO_DELETE")}</h2>
+                    <div className="gap-4 d-flex justify-content-between">
+                        <button type="button" onClick={()=> handleDeleteFile(confirmationModal?.fileId)} className="btn btn-danger px-5">{t("yes")}</button>
+                        <button type="button" onClick={()=>setConfirmationModal({value : !confirmationModal?.value, fileId: null})} className="theme-secondary-btn  px-5">{t("no")}</button>
+                    </div>
+                </div>
+            </ViewModal>
 
             {/* Model for send email */}
-
             <ViewModal
                 show={!!documentId}
                 onCloseClick={resetDocumentId}
