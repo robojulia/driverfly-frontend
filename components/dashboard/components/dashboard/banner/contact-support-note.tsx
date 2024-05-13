0d6026f9e16d5229bbeb9b172a5e53affbd79384
Khuram K <khurramk@codeupscale.com>
2024-05-13 13:37:06 +0500

    DRIV-1733 | feedack | change the wordings

--- components/dashboard/components/dashboard/banner/contact-support-note.tsx
+++ components/dashboard/components/dashboard/banner/contact-support-note.tsx
@@ -1,29 +1,33 @@
-import { useState } from "react";
+import Link from "next/link";
+import { OverlayTrigger, Popover } from "react-bootstrap";
 import { Question } from "react-bootstrap-icons";
 import { useAuth } from '../../../../../hooks/use-auth';
 import { useTranslation } from "../../../../../hooks/use-translation";
-import { OverlayTrigger, Tooltip } from "react-bootstrap";
-import OverlyPopover from "../../../../popover/overly-popover";
 
 export default function ContactSupportNote() {
 
     const { user } = useAuth();
     const { t } = useTranslation();
 
-    const [showMessage, setShowMessage] = useState<boolean>(false);
+    const popover = (
+        <Popover id="popover-basic">
+            <Popover.Header as="h3">{t("CONTACT_SUPPORT")}</Popover.Header>
+            <Popover.Body>
+                {t("CONTACT_SUPPORT_MESSAGE_THROUGH")} &nbsp;
+                <Link legacyBehavior href={`/dashboard/company/settings/support`}>{t("CONTACT_SUPPORT_MESSAGE_LINK")}</Link>&nbsp;
+                {t("CONTACT_SUPPORT_MESSAGE_EMAIL")}&nbsp; <Link legacyBehavior href={`mailto:help@driverfly.co`}>help@driverfly.co</Link>.
+            </Popover.Body>
+        </Popover>
+    );
 
     return (
         <>
             {
                 (!!user || !!user?.company) &&
-                <div className="contact-support" onMouseEnter={() => setShowMessage(true)} onMouseLeave={() => setShowMessage(false)}>
-                    <OverlyPopover
-                        placement="top"
-                        trigger={['hover', 'click']}
-                        str="CONTACT_SUPPORT">
+                <div className="contact-support">
+                    <OverlayTrigger trigger="click" placement="top-end" overlay={popover}>
                         <Question className="question-mark" />
-                    </OverlyPopover>
-
+                    </OverlayTrigger>
                 </div>
             }
         </>
