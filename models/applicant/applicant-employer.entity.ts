5140dab6c5bfd214e28742e7ccf267ae7b855edb
Rana Usama <usama.dilshad@codeupscale.com>
2024-07-23 16:59:53 +0500

    DRIV-1768 | Implement Functionality to Send VOE Request under Work History

--- models/applicant/applicant-employer.entity.ts
+++ models/applicant/applicant-employer.entity.ts
@@ -43,9 +43,46 @@ export class ApplicantEmployerEntity {
 			address: yup.string().optional().trim().nullable(),
 			address_2: yup.string().optional().trim().nullable(),
 			start_at: yup.date().max(new Date()).nullable(),
-			end_at: yup.date().when('is_current',{
+			end_at: yup.date().when('is_current', {
 				is: is_current => !is_current,
-				then : yup.date().required().test({
+				then: yup.date().required().test({
+					test: (value, context) => {
+						const start_date = context.resolve(yup.ref('start_at'));
+						if (!Boolean(value)) return true;
+						if (value > start_date) return true;
+
+						return context.createError({
+							path: context.path,
+							message: 'END_DATE_MUST_BE_AFTER_START_DATE'
+						})
+					}
+				}
+				)
+			}).nullable(),
+			title: yup.string().nullable().trim(),
+			street: yup.string().nullable().trim(),
+			city: yup.string().nullable().trim(),
+			state: yup.string().nullable(),
+			zip_code: yup.string().nullable(),
+			phone: yup.string().nullable(),
+			can_contact: yup.bool().nullable(),
+			is_subject_to_fmcsrs: yup.bool().nullable(),
+			is_subject_to_drug_tests: yup.bool().nullable(),
+			is_current: yup.boolean().nullable(),
+		});
+	}
+
+	static yupEditApplicantSchema() {
+		return yup.object({
+			name: yup.string().required().trim().nullable(),
+			manager_name: yup.string().optional().trim().nullable(),
+			email: yup.string().email().optional().nullable(),
+			address: yup.string().optional().trim().nullable(),
+			address_2: yup.string().optional().trim().nullable(),
+			start_at: yup.date().max(new Date()).nullable(),
+			end_at: yup.date().when('is_current', {
+				is: is_current => !is_current,
+				then: yup.date().test({
 					test: (value, context) => {
 						const start_date = context.resolve(yup.ref('start_at'));
 						if (!Boolean(value)) return true;
