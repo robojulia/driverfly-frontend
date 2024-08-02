809261af148bdb76e8cc789ef336a6b2688e1ee5
Rana Usama <usama.dilshad@codeupscale.com>
2024-08-02 17:01:49 +0500

    DRIV-1764 |  Import Applicant - Uploaded Documents #Update1

--- models/applicant/applicant.entity.ts
+++ models/applicant/applicant.entity.ts
@@ -1032,7 +1032,7 @@ export class ApplicantEntity {
   static yupSchemaForApplicantDocumentsForm() {
     return yup.object({
       documents: (
-        yup.array(DocumentEntity.yupSchema(ApplicantDocumentType)) as any
+        yup.array(DocumentEntity.yupSchema()) as any
       ).unique("type"),
     });
   }
