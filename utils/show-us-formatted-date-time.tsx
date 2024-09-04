cf4ae66481b8e54848d2c688648343250de74dbb
Khuram K <khurramk@codeupscale.com>
2024-09-04 16:23:50 +0500

    Hot Fixes

--- utils/show-us-formatted-date-time.tsx
+++ utils/show-us-formatted-date-time.tsx
@@ -1,7 +1,8 @@
 
 export function ShowUsFormattedDateTime(date: Date, skipTime?: boolean) {
     if (!date) return ``;
-    date = new Date(date);
+
+    date = typeof date == 'string' ? new Date(date) : date;
     const formattedDate = date?.toLocaleDateString('en-US', {
         month: '2-digit',
         day: '2-digit',
