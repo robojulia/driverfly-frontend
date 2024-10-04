01f9b9105f16e07a34e78e09fba311378841c393
Rana Usama <usama.dilshad@codeupscale.com>
2024-10-04 11:05:58 +0500

    DRIV-1807 | Job Posting Issues

--- components/forms/company/job-form.tsx
+++ components/forms/company/job-form.tsx
@@ -91,8 +91,8 @@ export function JobForm(props: JobFormProps) {
     useEffect(() => {
         form.setValues({
             ...(entity || {}),
-            schedule: entity?.schedule || JobSchedule.OPEN_TO_NEGOTIATE,
-            pay_method: entity?.pay_method || JobPayMethod.OPEN_TO_NEGOTIATE,
+            schedule: entity?.schedule,
+            pay_method: entity?.pay_method,
             min_weekly_pay: Boolean(entity?.min_weekly_pay)
                 ? entity.min_weekly_pay
                 : null,
@@ -944,6 +944,7 @@ export function JobForm(props: JobFormProps) {
                                         name="cdl_class"
                                         labelPrefix="DriverLicenseType"
                                         required
+                                        displayPlaceholder
                                         enumType={DriverLicenseType}
                                         formik={form}
                                     />
