0b34ab779ce56bdb3a9cbfd86d47daa41e8ca4cf
Adam <akirbsy@gmail.com>
2025-06-11 11:15:37 -0700

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,8 @@ module.exports = {
   extends: ['next/core-web-vitals', 'prettier'],
   rules: {
     'no-unused-vars': 'off',
-    indent: ['error', 2, { SwitchCase: 1 }],
-    'no-tabs': 'error',
+    indent: ['warn', 2, { SwitchCase: 1 }],
+    'no-tabs': 'warn',
     'react/jsx-indent': ['error', 2],
     'react/jsx-indent-props': ['error', 2],
   },
