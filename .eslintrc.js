93b4994f4378d9dbd7c437a214d2ef18f6bb2521
Adam <akirbsy@gmail.com>
2025-06-11 11:31:31 -0700

    turning off indent linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,9 @@ module.exports = {
   extends: ['next/core-web-vitals', 'prettier'],
   rules: {
     'no-unused-vars': 'off',
-    indent: ['warn', 2, { SwitchCase: 1 }],
-    'no-tabs': 'warn',
-    'react/jsx-indent': ['warn', 2],
-    'react/jsx-indent-props': ['warn', 2],
+    indent: ['off', 2, { SwitchCase: 1 }],
+    'no-tabs': 'off',
+    'react/jsx-indent': ['off', 2],
+    'react/jsx-indent-props': ['off', 2],
   },
 };
