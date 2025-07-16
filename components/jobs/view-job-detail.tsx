699e994374822d046ca6971035ed8aeb16430b7c
Chris Jennison <cjennison92@gmail.com>
2025-07-16 07:59:01 -0400

    add apply button to job board

--- components/jobs/view-job-detail.tsx
+++ components/jobs/view-job-detail.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import React from 'react';
 import { Button, Col, Container, Row } from 'react-bootstrap';
-import { ArrowRight, CurrencyDollar } from "react-bootstrap-icons";
+import { ArrowRight, CurrencyDollar } from 'react-bootstrap-icons';
 import { useAuth } from '../../hooks/use-auth';
 import { useTranslation } from '../../hooks/use-translation';
 import { ViewJobDetailProps } from '../../types/job/view-job-detail-props.type';
@@ -19,140 +19,155 @@ import { isExpired } from '../../utils/date';
 import { ShowUsFormattedDateTime } from '../../utils/show-us-formatted-date-time';
 
 export default function ViewJobDetail(props: ViewJobDetailProps) {
+  const {
+    className,
+    job,
+    relatedJobs,
+    canApply,
+    canSave,
+    hideVehicles,
+    viewAllJobsLink,
+    hideCompanyName,
+    hideSocialLinks,
+    quick_apply,
+  } = props;
 
-    const {
-        className,
-        job,
-        relatedJobs,
-        canApply,
-        canSave,
-        hideVehicles,
-        viewAllJobsLink,
-        hideCompanyName,
-        hideSocialLinks,
-        quick_apply
-    } = props
+  const { t } = useTranslation();
+  const [encourageModal, setEncourageModal] = React.useState(false);
+  const { user, company } = useAuth();
 
-    const { t } = useTranslation();
-    const [encourageModal, setEncourageModal] = React.useState(false)
-    const { user, company } = useAuth();
+  console.log(job);
 
-    return (
-        <section className="top-links-sec ort-general p-3 vehicle-img">
+  return (
+    <section className="top-links-sec ort-general p-3 vehicle-img">
+      <FlagJob jobId={job.id} />
 
-            <FlagJob jobId={job.id} />
-
-            <Container className='p-0'>
-                <Row>
-                    <Col md={9}>
-                        <div className="ort-inner ">
-                            <div className=" company_photo media align-items-center bg-transparent border-0 p-0">
-                                <span className="text-dark text-center text-decoration-none">
-                                    <CompanyPhoto className="d-flex mr-4 truck-img mb-3" company={job.company} />
-                                    {viewAllJobsLink &&
-                                        <Link href={viewAllJobsLink}>
-                                            <a style={{ color: "black" }}>
-                                                {t('view_all_jobs')} <ArrowRight className="pl-1" />
-                                            </a>
-                                        </Link>
-                                    }
-                                </span>
-                                <div className="media-body">
-                                    <h4 className="mt-0">
-                                        {job.title} {" "}
-
-                                        {isExpired(job.expiry_date) && (
-                                            <span className='text-danger'>({t("EXPIRED")})</span>
-                                        )}
-
-                                    </h4>
-                                    <div className="job-date-author">
-                                        <ShowFormattedDate
-                                            date={job.created_at}
-                                            showTimeSince
-                                            labelPrefix="POSTED"
-                                            labelPostfix='AGO'
-                                        />
-                                        {
-                                            (!!!hideCompanyName && job.company?.name) &&
-                                            <>
-                                                {t('BY')} <Link href={`/employer/${job?.company?.slug}`}>
-                                                    <span role="button" className="employer text-theme">{job?.company?.name}</span>
-                                                </Link>
-                                            </>
-                                        }
-                                    </div>
-                                    <div className="job-metas">
-                                        <div className="job-location d-flex align-items-center">
-                                            <p className="pr-4 m-0">{buildAddress(job.location)}</p>
-                                        </div>
-                                        <div >
-                                            <p><CurrencyDollar />{job.min_weekly_pay || 0} - {job.max_weekly_pay || 0} {t('PER_WEEK')}</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </Col>
-                    <Col md={3}>
-                        {!!quick_apply
-                            ? <div className="ort-btn mt-lg-4 mt-0">
-                                <Link href={`/apply/suggested-job/${quick_apply}/${job.id}`}>
-                                    <button type="button" className="btn theme-primary-btn" > {t('QUICK_APPLY')}<ArrowRight /></button>
-                                </Link>
-                            </div>
-                            : !company?.id && canApply && <JobApply setEncourageModal={setEncourageModal} job={job} />
-                        }
-                        {!company?.id && canSave && <SaveJob job={job} />}
-                    </Col>
-                </Row>
-            </Container>
-
-            <div className="job-deatails-sec">
-                <Container className='p-0'>
-                    <Row>
-                        <Col lg={8}>
-                            < JobDescription job={job} />
-                            {!!!hideVehicles && < JobVehicles job={job} />}
-                            {/* {!!!hideSocialLinks && < SocilShare />} */}
-                            {relatedJobs || <></>}
-                        </Col>
-                        <Col lg={4}>
-                            < JonInformation job={job} />
-                        </Col>
-                    </Row>
-                </Container>
+      <Container className="p-0">
+        <Row>
+          <Col md={9}>
+            <div className="ort-inner ">
+              <div className=" company_photo media align-items-center bg-transparent border-0 p-0">
+                <span className="text-dark text-center text-decoration-none">
+                  <CompanyPhoto className="d-flex mr-4 truck-img mb-3" company={job.company} />
+                  {viewAllJobsLink && (
+                    <Link href={viewAllJobsLink}>
+                      <a style={{ color: 'black' }}>
+                        {t('view_all_jobs')} <ArrowRight className="pl-1" />
+                      </a>
+                    </Link>
+                  )}
+                </span>
+                <div className="media-body">
+                  <h4 className="mt-0">
+                    {job.title}{' '}
+                    {isExpired(job.expiry_date) && (
+                      <span className="text-danger">({t('EXPIRED')})</span>
+                    )}
+                  </h4>
+                  <div className="job-date-author">
+                    <ShowFormattedDate
+                      date={job.created_at}
+                      showTimeSince
+                      labelPrefix="POSTED"
+                      labelPostfix="AGO"
+                    />
+                    {!!!hideCompanyName && job.company?.name && (
+                      <>
+                        {t('BY')}{' '}
+                        <Link href={`/employer/${job?.company?.slug}`}>
+                          <span role="button" className="employer text-theme">
+                            {job?.company?.name}
+                          </span>
+                        </Link>
+                      </>
+                    )}
+                  </div>
+                  <div className="job-metas">
+                    <div className="job-location d-flex align-items-center">
+                      <p className="pr-4 m-0">{buildAddress(job.location)}</p>
+                    </div>
+                    <div>
+                      <p>
+                        <CurrencyDollar />
+                        {job.min_weekly_pay || 0} - {job.max_weekly_pay || 0} {t('PER_WEEK')}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-            <ViewModal
-                show={user == null && encourageModal == true}
-                closeText="CANCEL"
+          </Col>
+          <Col md={3}>
+            {!!quick_apply ? (
+              <div className="ort-btn mt-lg-4 mt-0">
+                <Link href={`/apply/suggested-job/${quick_apply}/${job.id}`}>
+                  <button type="button" className="btn theme-primary-btn">
+                    {' '}
+                    {t('QUICK_APPLY')}
+                    <ArrowRight />
+                  </button>
+                </Link>
+              </div>
+            ) : (
+              !company?.id &&
+              canApply && <JobApply setEncourageModal={setEncourageModal} job={job} />
+            )}
+
+            {/* Direct Application Link */}
+            {canApply && job.company?.slug && (
+              <div className="ort-btn mt-3">
+                <Link href={`/apply/${job.company.slug}?jobId=${job.id}`}>
+                  <button type="button" className="btn btn-outline-primary w-100">
+                    Apply now <ArrowRight />
+                  </button>
+                </Link>
+              </div>
+            )}
+
+            {!company?.id && canSave && <SaveJob job={job} />}
+          </Col>
+        </Row>
+      </Container>
+
+      <div className="job-deatails-sec">
+        <Container className="p-0">
+          <Row>
+            <Col lg={8}>
+              <JobDescription job={job} />
+              {!!!hideVehicles && <JobVehicles job={job} />}
+              {/* {!!!hideSocialLinks && < SocilShare />} */}
+              {relatedJobs || <></>}
+            </Col>
+            <Col lg={4}>
+              <JonInformation job={job} />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <ViewModal show={user == null && encourageModal == true} closeText="CANCEL">
+        <>
+          <Row>
+            <p
+              className="d-flex justify-content-center align-items-center text-green-500"
+              style={{ color: 'green' }}
             >
-                <>
-                    <Row >
-                        <p className='d-flex justify-content-center align-items-center text-green-500' style={{ color: "green" }}>
-                            <b>
-                                {t('CONGRATS_ON_APPLYING')}
-                            </b>
-                        </p>
-                    </Row>
-                    <Row >
-                        <div className='mb-2 d-flex justify-content-center align-items-center'>
-                            <Button href='/signup'>
-                                {t('PLEASE_PROCEED_WITH_REGISTRATION')}
-                            </Button>
-                        </div>
-                    </Row>
-                    <Row>
-                        <div className='mt-2 d-flex justify-content-center align-items-center'>
-                            <p>
-                                <b>
-                                    {t('GET_REGISTERED_MESSAGE_QUICK_APPLY')}
-                                </b>
-                            </p>
-                        </div>
-                    </Row>
-                </>
-            </ViewModal>
-        </section>
-    )
+              <b>{t('CONGRATS_ON_APPLYING')}</b>
+            </p>
+          </Row>
+          <Row>
+            <div className="mb-2 d-flex justify-content-center align-items-center">
+              <Button href="/signup">{t('PLEASE_PROCEED_WITH_REGISTRATION')}</Button>
+            </div>
+          </Row>
+          <Row>
+            <div className="mt-2 d-flex justify-content-center align-items-center">
+              <p>
+                <b>{t('GET_REGISTERED_MESSAGE_QUICK_APPLY')}</b>
+              </p>
+            </div>
+          </Row>
+        </>
+      </ViewModal>
+    </section>
+  );
 }
--- pages/jobs/[jobId]/[slug].tsx
+++ pages/jobs/[jobId]/[slug].tsx
@@ -1,16 +1,15 @@
-import ViewJobDetail from "../../../components/jobs/view-job-detail";
-import { PublicLayout } from "../../../components/layouts/public-layout";
+import ViewJobDetail from '../../../components/jobs/view-job-detail';
+import { PublicLayout } from '../../../components/layouts/public-layout';
 import RelatedJobs from '../../../components/related-jobs/related-jobs';
-import StructuredData from "../../../components/seo/structured-data";
-import { useTranslation } from "../../../hooks/use-translation";
-import { JobEntity } from "../../../models/job/job.entity";
-import { JobDetailProps } from "../../../types/job/job-detail-props.type";
-import { Pagination } from "../../../types/pagination.type";
-import JobApi from "../../api/job";
+import StructuredData from '../../../components/seo/structured-data';
+import { useTranslation } from '../../../hooks/use-translation';
+import { JobEntity } from '../../../models/job/job.entity';
+import { JobDetailProps } from '../../../types/job/job-detail-props.type';
+import { Pagination } from '../../../types/pagination.type';
+import JobApi from '../../api/job';
 
 export default function Detail({ job, relatedJobs, quick_apply, error }: JobDetailProps) {
-
-  console.error("error", error);
+  console.error('error', error);
 
   const { t } = useTranslation();
 
@@ -20,7 +19,14 @@ export default function Detail({ job, relatedJobs, quick_apply, error }: JobDeta
       <ViewJobDetail
         quick_apply={quick_apply}
         job={job}
-        relatedJobs={< RelatedJobs jobs={relatedJobs} jobLink="jobs" hideCompanyName={false} jobLinkSlugable={true} />}
+        relatedJobs={
+          <RelatedJobs
+            jobs={relatedJobs}
+            jobLink="jobs"
+            hideCompanyName={false}
+            jobLinkSlugable={true}
+          />
+        }
         canApply={true}
         canSave={true}
         hideVehicles={false}
@@ -28,34 +34,32 @@ export default function Detail({ job, relatedJobs, quick_apply, error }: JobDeta
         viewAllJobsLink={`/find-jobs?companyId=${job.company?.id}`}
       />
     </>
-  )
+  );
 }
 export async function getServerSideProps({ params, query }) {
   try {
     const { jobId } = params;
     const { quick_apply } = query;
 
-    if (!!!jobId)
-      return { notFound: true }
+    if (!!!jobId) return { notFound: true };
 
-    const job = await new JobApi().getById(jobId)
-    if (!!!job)
-      return { notFound: true }
+    const job = await new JobApi().getById(jobId);
+    if (!!!job) return { notFound: true };
 
-    const { items } = await new JobApi().search({ exclude: { jobId: jobId }, companyId: job.company?.id, take: 3 }) as Pagination<JobEntity>;
+    const { items } = (await new JobApi().search({
+      exclude: { jobId: jobId },
+      companyId: job.company?.id,
+      take: 3,
+    })) as Pagination<JobEntity>;
     return {
-      props: { job: job, relatedJobs: items, quick_apply: quick_apply || null }
-    }
+      props: { job: job, relatedJobs: items, quick_apply: quick_apply || null },
+    };
   } catch (error) {
-    console.error("Exception is here:", error?.response?.data);
-    return { props: { job: {}, relatedJobs: [], error: error?.response?.data } }
+    console.error('Exception is here:', error?.response?.data);
+    return { props: { job: {}, relatedJobs: [], error: error?.response?.data } };
   }
 }
 
 Detail.getLayout = function getLayout(page) {
-  return (
-    <PublicLayout>
-      {page}
-    </PublicLayout>
-  )
-}
+  return <PublicLayout>{page}</PublicLayout>;
+};
