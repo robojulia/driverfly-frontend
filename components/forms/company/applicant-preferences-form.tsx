6d7e07c53a02980f880a4b881b5133f46cfae09f
Adam <akirbsy@gmail.com>
2025-11-26 23:11:27 -0800

    trigger redeploy

--- components/forms/company/applicant-preferences-form.tsx
+++ components/forms/company/applicant-preferences-form.tsx
@@ -64,7 +64,7 @@ export function ApplicantPreferencesForm(props: ApplicantPreferencesFormProps) {
   });
 
   // Load form values on initial mount only
-  // Don't reload on entity updates to prevent overwriting user changes
+  // Don't reload on entity updates to prevent ove rwriting user changes
   // (backend doesn't return routes/preferred_location with withRelations, which would reset form to empty values)
   useEffect(() => {
     if (!!entity?.id && !initialized) {
