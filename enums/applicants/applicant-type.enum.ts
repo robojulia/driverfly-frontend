de944a5a12a34bada983dfc8f72c262d05d98306
Adam <akirbsy@gmail.com>
2026-01-08 10:34:26 -0800

    enum fixes

--- enums/applicants/applicant-type.enum.ts
+++ enums/applicants/applicant-type.enum.ts
@@ -4,4 +4,6 @@ export enum ApplicantType {
   DHA = 'DHA', // through Digital Hiring App
   DIRECT_JOB_APPLY = 'DIRECT_JOB_APPLY', // Directly applied on job
   AUTO_RECRUIT = 'AUTO_RECRUIT',
+  JOB_BOARD = 'JOB_BOARD',
+  SOCIAL_MEDIA = 'SOCIAL_MEDIA',
 }
